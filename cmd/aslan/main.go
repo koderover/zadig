dd7c853c0b216496f76583b0e8a3b6c558401a2f
yelei <yelei@koderover.com>
2021-05-17 12:43:45 +0800

    test zadig ci webhook
    Signed-off-by: yelei <yelei@koderover.com>

--- cmd/aslan/main.go
+++ cmd/aslan/main.go
@@ -26,7 +26,7 @@ import (
 	"github.com/koderover/zadig/lib/microservice/aslan/server"
 )
 
-// webhook test new
+// webhook test
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
