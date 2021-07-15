a6a84844a8ba17dd846b2a980beae51ce3840176
Guang Jiong Lou <7991675+27149chen@users.noreply.github.com>
2021-07-15 11:06:14 +0800

    remove incorrect ut (#81)

--- pkg/microservice/aslan/core/common/service/registry/service_test.go
+++ pkg/microservice/aslan/core/common/service/registry/service_test.go
@@ -17,156 +17,138 @@ limitations under the License.
 package registry
 
 import (
-	"reflect"
-	"sort"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-
-	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
-	"github.com/koderover/zadig/pkg/tool/log"
 	_ "github.com/koderover/zadig/pkg/util/testing"
 )
 
-func Test_v2RegistryService_ListRepoImages(t *testing.T) {
-	assert := require.New(t)
-	type args struct {
-		option ListRepoImagesOption
-		log    *zap.SugaredLogger
-	}
-	tests := []struct {
-		name     string
-		s        *v2RegistryService
-		args     args
-		wantResp *ReposResp
-		wantErr  bool
-	}{
-		{
-			"list tags of library",
-			&v2RegistryService{},
-			args{
-				ListRepoImagesOption{
-					Endpoint{
-						"https://n7832lxy.mirror.aliyuncs.com",
-						"",
-						"",
-						"",
-						"library",
-					},
-					[]string{"mysql", "alpine"},
-				},
-				log.SugaredLogger(),
-			},
-			&ReposResp{Total: 2},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &v2RegistryService{}
-			gotResp, err := s.ListRepoImages(tt.args.option, tt.args.log)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("v2RegistryService.ListRepoImages() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			assert.Nil(err)
-			assert.Equal(gotResp.Total, tt.wantResp.Total)
-			if tt.args.option.Addr == "https://n7832lxy.mirror.aliyuncs.com" {
-				for _, repo := range gotResp.Repos {
-					assert.Contains(repo.Tags, "latest")
-				}
-			}
-		})
-	}
-}
-
-func Test_v2RegistryService_GetImageInfo(t *testing.T) {
-	type args struct {
-		option GetRepoImageDetailOption
-		log    *zap.SugaredLogger
-	}
-	tests := []struct {
-		name    string
-		s       *v2RegistryService
-		args    args
-		wantDi  *models.DeliveryImage
-		wantErr bool
-	}{
-		{
-			"list tag info of mysql:5.7",
-			&v2RegistryService{},
-			args{
-				GetRepoImageDetailOption{
-					Endpoint{
-						"https://n7832lxy.mirror.aliyuncs.com",
-						"",
-						"",
-						"",
-						"library",
-					},
-					"mysql",
-					"5.7.23",
-				},
-				log.SugaredLogger(),
-			},
-			&models.DeliveryImage{ImageDigest: "sha256:953b53af26805d82eca95f28df6ae82e8e15cd1e587b4c5cd06a78be80e84050"},
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := &v2RegistryService{}
-			gotDi, err := s.GetImageInfo(tt.args.option, tt.args.log)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("v2RegistryService.GetImageInfo() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if err == nil {
-				if gotDi.ImageDigest != tt.wantDi.ImageDigest {
-					t.Errorf("v2RegistryService.GetImageInfo() = %v, want %v", gotDi, tt.wantDi)
-				}
-			}
-		})
-	}
-}
-
-func TestReverseStringSlice_Len(t *testing.T) {
-	a := []string{"1", "3", "2"}
-	sort.Sort(ReverseStringSlice(a))
-	if !reflect.DeepEqual(a, []string{"2", "3", "1"}) {
-		t.Error("reverse sort not works")
-	}
-}
-
-func TestSwrListRepoImage(t *testing.T) {
-	s := &SwrService{}
-	listRepoImagesOption := ListRepoImagesOption{
-		Endpoint: Endpoint{
-			Namespace: "lilian",
-			Ak:        "",
-			Sk:        "",
-		},
-		Repos: []string{"nginx-test"},
-	}
-	_, err := s.ListRepoImages(listRepoImagesOption, log.SugaredLogger())
-	assert.Nil(t, err)
-}
-
-func TestSwrImageInfo(t *testing.T) {
-	s := &SwrService{}
-	getRepoImageDetailOption := GetRepoImageDetailOption{
-		Endpoint: Endpoint{
-			Region:    "cn-north-4",
-			Namespace: "lilian",
-			Ak:        "",
-			Sk:        "",
-		},
-		Tag:   "20210712210942-34-master",
-		Image: "nginx-test",
-	}
-	_, err := s.GetImageInfo(getRepoImageDetailOption, log.SugaredLogger())
-	assert.Nil(t, err)
-}
+//func Test_v2RegistryService_ListRepoImages(t *testing.T) {
+//	assert := require.New(t)
+//	type args struct {
+//		option ListRepoImagesOption
+//		log    *zap.SugaredLogger
+//	}
+//	tests := []struct {
+//		name     string
+//		s        *v2RegistryService
+//		args     args
+//		wantResp *ReposResp
+//		wantErr  bool
+//	}{
+//		{
+//			"list tags of library",
+//			&v2RegistryService{},
+//			args{
+//				ListRepoImagesOption{
+//					Endpoint{
+//						"https://n7832lxy.mirror.aliyuncs.com",
+//						"",
+//						"",
+//						"",
+//						"library",
+//					},
+//					[]string{"mysql", "alpine"},
+//				},
+//				log.SugaredLogger(),
+//			},
+//			&ReposResp{Total: 2},
+//			false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			s := &v2RegistryService{}
+//			gotResp, err := s.ListRepoImages(tt.args.option, tt.args.log)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("v2RegistryService.ListRepoImages() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//
+//			assert.Nil(err)
+//			assert.Equal(gotResp.Total, tt.wantResp.Total)
+//			if tt.args.option.Addr == "https://n7832lxy.mirror.aliyuncs.com" {
+//				for _, repo := range gotResp.Repos {
+//					assert.Contains(repo.Tags, "latest")
+//				}
+//			}
+//		})
+//	}
+//}
+//
+//func Test_v2RegistryService_GetImageInfo(t *testing.T) {
+//	type args struct {
+//		option GetRepoImageDetailOption
+//		log    *zap.SugaredLogger
+//	}
+//	tests := []struct {
+//		name    string
+//		s       *v2RegistryService
+//		args    args
+//		wantDi  *models.DeliveryImage
+//		wantErr bool
+//	}{
+//		{
+//			"list tag info of mysql:5.7",
+//			&v2RegistryService{},
+//			args{
+//				GetRepoImageDetailOption{
+//					Endpoint{
+//						"https://n7832lxy.mirror.aliyuncs.com",
+//						"",
+//						"",
+//						"",
+//						"library",
+//					},
+//					"mysql",
+//					"5.7.23",
+//				},
+//				log.SugaredLogger(),
+//			},
+//			&models.DeliveryImage{ImageDigest: "sha256:953b53af26805d82eca95f28df6ae82e8e15cd1e587b4c5cd06a78be80e84050"},
+//			false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			s := &v2RegistryService{}
+//			gotDi, err := s.GetImageInfo(tt.args.option, tt.args.log)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("v2RegistryService.GetImageInfo() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if err == nil {
+//				if gotDi.ImageDigest != tt.wantDi.ImageDigest {
+//					t.Errorf("v2RegistryService.GetImageInfo() = %v, want %v", gotDi, tt.wantDi)
+//				}
+//			}
+//		})
+//	}
+//}
+//
+//func TestSwrListRepoImage(t *testing.T) {
+//	s := &SwrService{}
+//	listRepoImagesOption := ListRepoImagesOption{
+//		Endpoint: Endpoint{
+//			Namespace: "lilian",
+//			Ak:        "",
+//			Sk:        "",
+//		},
+//		Repos: []string{"nginx-test"},
+//	}
+//	_, err := s.ListRepoImages(listRepoImagesOption, log.SugaredLogger())
+//	assert.Nil(t, err)
+//}
+//
+//func TestSwrImageInfo(t *testing.T) {
+//	s := &SwrService{}
+//	getRepoImageDetailOption := GetRepoImageDetailOption{
+//		Endpoint: Endpoint{
+//			Region:    "cn-north-4",
+//			Namespace: "lilian",
+//			Ak:        "",
+//			Sk:        "",
+//		},
+//		Tag:   "20210712210942-34-master",
+//		Image: "nginx-test",
+//	}
+//	_, err := s.GetImageInfo(getRepoImageDetailOption, log.SugaredLogger())
+//	assert.Nil(t, err)
+//}
