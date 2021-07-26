69d75d5a68e88556f63ea4a533be41289c6684cf
liu deyi <andrew@koderover.com>
2021-07-26 22:14:17 +0800

    debug codehub webhook
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/aslan/core/common/service/codehub/webhook.go
+++ pkg/microservice/aslan/core/common/service/codehub/webhook.go
@@ -1,12 +1,16 @@
 package codehub
 
 import (
+	"fmt"
+
 	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	gitservice "github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/git"
 	"github.com/koderover/zadig/pkg/tool/codehub"
 )
 
 func (c *Client) CreateWebHook(owner, repo string) (string, error) {
+	fmt.Println(fmt.Sprintf("owner:%s", owner))
+	fmt.Println(fmt.Sprintf("repo:%s", repo))
 	return c.AddWebhook(owner, repo, &codehub.AddCodehubHookPayload{
 		HookURL:    config.WebHookURL(),
 		Token:      gitservice.GetHookSecret(),
