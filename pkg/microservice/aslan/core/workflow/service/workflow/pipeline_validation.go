63050fb0f8cbebbef14a36655aea0d4ca3566bdb
mouuii <zhouchengbin@koderover.com>
2021-07-27 13:56:33 +0800

    workflow commit bug
    Signed-off-by: mouuii <zhouchengbin@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/pipeline_validation.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/pipeline_validation.go
@@ -391,7 +391,7 @@ func setBuildInfo(build *types.Repository) {
 				})
 				if err == nil && len(prCommits) > 0 {
 					for _, commit := range prCommits {
-						build.CommitID = *commit.Commit.Tree.SHA
+						build.CommitID = *commit.SHA
 						build.CommitMessage = *commit.Commit.Message
 						build.AuthorName = *commit.Commit.Author.Name
 						return
