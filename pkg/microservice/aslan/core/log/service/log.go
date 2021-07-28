9751a1d78b20d84bb7f3f983fcbdb6c6345f8cb3
tommydong <dongchangwei@koderover.com>
2021-07-28 19:12:36 +0800

    add more log for debug
    Signed-off-by: tommydong <dongchangwei@koderover.com>

--- pkg/microservice/aslan/core/log/service/log.go
+++ pkg/microservice/aslan/core/log/service/log.go
@@ -154,7 +154,7 @@ func GetPodLogByHttp(podName, containerName, envName, productName string, tail i
 		return "", err
 	}
 	str := buf.String()
-
+	log.Infof("GetPodLogByHttp log:%v",str)
 	return str, nil
 
 	/*cmdStr := ""
