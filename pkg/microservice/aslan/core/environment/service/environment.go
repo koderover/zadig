8f3c8371f8a1083d90c29918882cd92d00654932
allenshen <shendongdong@koderover.com>
2021-08-26 18:49:39 +0800

    remove annotated code

--- pkg/microservice/aslan/core/environment/service/environment.go
+++ pkg/microservice/aslan/core/environment/service/environment.go
@@ -642,7 +642,6 @@ func UpdateProduct(existedProd, updateProd *commonmodels.Product, renderSet *com
 							lock.Lock()
 							switch e := err.(type) {
 							case *multierror.Error:
-								//errList = multierror.Append(errList, e.Errors...)
 								errList = multierror.Append(errList, errors.New(e.Error()))
 							default:
 								errList = multierror.Append(errList, e)
