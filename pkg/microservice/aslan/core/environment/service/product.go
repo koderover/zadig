2ac8682ad6b1d55e9c4691e5b2db3c2191d10d0f
mouuii <49775493+mouuii@users.noreply.github.com>
2021-09-10 17:15:15 +0800

    fix pm create namespace bug (#294)
    * refactor pm
    
    Signed-off-by: mouuii <zhouchengbin@koderover.com>
    
    * refactor pm
    
    Signed-off-by: mouuii <zhouchengbin@koderover.com>
    
    Co-authored-by: mouuii <zhouchengbin@koderover.com>

--- pkg/microservice/aslan/core/environment/service/product.go
+++ pkg/microservice/aslan/core/environment/service/product.go
@@ -116,6 +116,9 @@ func GetInitProduct(productTmplName string, log *zap.SugaredLogger) (*commonmode
 	ret.Render = &commonmodels.RenderInfo{Name: "", Description: ""}
 	ret.Vars = prodTmpl.Vars
 	ret.ChartInfos = prodTmpl.ChartInfos
+	if prodTmpl.ProductFeature.BasicFacility == setting.BasicFacilityCVM {
+		ret.Source = setting.PMDeployType
+	}
 
 	allServiceInfoMap := prodTmpl.AllServiceInfoMap()
 	for _, names := range prodTmpl.Services {
