c0528450fe184dc95321e867373c8f73c6c50d82
liu deyi <andrew@koderover.com>
2021-10-28 21:07:08 +0800

    fix artifact deploy bug
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/aslan/core/workflow/testing/service/workspace.go
+++ pkg/microservice/aslan/core/workflow/testing/service/workspace.go
@@ -21,19 +21,18 @@ import (
 
 	"go.uber.org/zap"
 
-	commonmodels "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/s3"
 	"github.com/koderover/zadig/pkg/setting"
 	s3tool "github.com/koderover/zadig/pkg/tool/s3"
 )
 
-func GetTestArtifactInfo(pipelineName, dir string, taskID int64, log *zap.SugaredLogger) (*commonmodels.S3Storage, []string, error) {
+func GetTestArtifactInfo(pipelineName, dir string, taskID int64, log *zap.SugaredLogger) ([]string, error) {
 	fis := make([]string, 0)
 
 	storage, err := s3.FindDefaultS3()
 	if err != nil {
 		log.Errorf("GetTestArtifactInfo FindDefaultS3 err:%v", err)
-		return nil, fis, nil
+		return fis, nil
 	}
 	if storage.Subfolder != "" {
 		storage.Subfolder = fmt.Sprintf("%s/%s/%d/%s", storage.Subfolder, pipelineName, taskID, "artifact")
@@ -47,13 +46,13 @@ func GetTestArtifactInfo(pipelineName, dir string, taskID int64, log *zap.Sugare
 	client, err := s3tool.NewClient(storage.Endpoint, storage.Ak, storage.Sk, storage.Insecure, forcedPathStyle)
 	if err != nil {
 		log.Errorf("GetTestArtifactInfo create s3 client err:%v", err)
-		return nil, fis, nil
+		return fis, nil
 	}
 	prefix := storage.GetObjectPath(dir)
 	files, err := client.ListFiles(storage.Bucket, prefix, true)
 	if err != nil || len(files) <= 0 {
 		log.Errorf("GetTestArtifactInfo ListFiles err:%v", err)
-		return nil, fis, nil
+		return fis, nil
 	}
-	return storage.S3Storage, files, nil
+	return files, nil
 }
