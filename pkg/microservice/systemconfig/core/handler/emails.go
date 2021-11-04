4d9e7f1310f2e1d184448636756a66f9bdabbd5c
mouuii <zhouchengbin@koderover.com>
2021-11-04 10:35:01 +0800

    poetry
    Signed-off-by: mouuii <zhouchengbin@koderover.com>

--- pkg/microservice/systemconfig/core/handler/emails.go
+++ pkg/microservice/systemconfig/core/handler/emails.go
@@ -47,33 +47,33 @@ func DeleteEmailHost(c *gin.Context) {
 func GetEmailService(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
-	ctx.Resp, ctx.Err = service.GetEmailHost(ctx.Logger)
+	ctx.Resp, ctx.Err = service.GetEmailService(ctx.Logger)
 }
 
 func CreateEmailService(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
-	req := new(models.EmailHost)
+	req := new(models.EmailService)
 	if err := c.ShouldBindJSON(req); err != nil {
 		ctx.Err = err
 		return
 	}
-	ctx.Resp, ctx.Err = service.CreateEmailHost(req, ctx.Logger)
+	ctx.Resp, ctx.Err = service.CreateEmailService(req, ctx.Logger)
 }
 
 func UpdateEmailService(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
-	req := new(models.EmailHost)
+	req := new(models.EmailService)
 	if err := c.ShouldBindJSON(req); err != nil {
 		ctx.Err = err
 		return
 	}
-	ctx.Resp, ctx.Err = service.UpdateEmailHost(req, ctx.Logger)
+	ctx.Resp, ctx.Err = service.UpdateEmailService(req, ctx.Logger)
 }
 
 func DeleteEmailService(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
-	ctx.Err = service.DeleteEmailHost(ctx.Logger)
+	ctx.Err = service.DeleteEmailService(ctx.Logger)
 }
