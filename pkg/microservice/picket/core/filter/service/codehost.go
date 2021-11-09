cd336f5046b1bd4b1c60aca7f48ea4abe22af277
mouuii <zhouchengbin@koderover.com>
2021-11-09 21:13:54 +0800

    hide secret
    Signed-off-by: mouuii <zhouchengbin@koderover.com>

---
+++ pkg/microservice/picket/core/filter/service/codehost.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/koderover/zadig/pkg/shared/client/systemconfig"
+)
+
+func ListCodeHost(_ *zap.SugaredLogger) ([]*systemconfig.CodeHost, error) {
+	list, err := systemconfig.GetCodeHostList()
+	if err != nil {
+		return nil, err
+	}
+	for k, _ := range list {
+		list[k].AccessKey = "***"
+		list[k].SecretKey = "***"
+		list[k].AccessToken = "***"
+	}
+	return list, nil
+}
