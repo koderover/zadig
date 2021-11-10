0b96bb4165af9c7881ae59f9d90ee5aba2b5d4e9
lou <alex1988@outlook.com>
2021-11-10 17:03:28 +0800

    update after review
    Signed-off-by: lou <alex1988@outlook.com>

--- pkg/microservice/user/core/service/login/third_party.go
+++ pkg/microservice/user/core/service/login/third_party.go
@@ -1,13 +1,19 @@
 package login
 
-import "github.com/koderover/zadig/pkg/shared/client/systemconfig"
+import (
+	"github.com/koderover/zadig/pkg/shared/client/systemconfig"
+	"github.com/koderover/zadig/pkg/tool/log"
+)
 
 type enabledStatus struct {
 	Enabled bool `json:"enabled"`
 }
 
 func ThirdPartyLoginEnabled() *enabledStatus {
-	connectors, _ := systemconfig.New().ListConnectors()
+	connectors, err := systemconfig.New().ListConnectors()
+	if err != nil {
+		log.Warnf("Failed to list connectors, err: %s", err)
+	}
 	return &enabledStatus{
 		Enabled: len(connectors) > 0,
 	}
