5848520d1cdc0ed417eaeb7017758352d2788d31
Min Min <minmin@koderover.com>
2021-11-18 03:25:20 +0800

    fix wrong package import
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/common/repository/mongodb/external_system.go
+++ pkg/microservice/aslan/core/common/repository/mongodb/external_system.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/globalsign/mgo/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
