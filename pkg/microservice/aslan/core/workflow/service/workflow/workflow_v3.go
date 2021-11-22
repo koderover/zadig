473caa84af3e0f493da1dacd88375bd44bec2add
Min Min <minmin@koderover.com>
2021-11-22 15:22:28 +0800

    changed type from interface{} to string
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/workflow_v3.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/workflow_v3.go
@@ -225,7 +225,7 @@ func GetWorkflowV3Args(id string, logger *zap.SugaredLogger) ([]*WorkflowV3TaskA
 	return resp, nil
 }
 
-func getEnvsFromExternalSystem(setting *commonmodels.ExternalSetting) ([]map[string]interface{}, error) {
+func getEnvsFromExternalSystem(setting *commonmodels.ExternalSetting) ([]map[string]string, error) {
 	externalSystem, err := commonrepo.NewExternalSystemColl().GetByID(setting.SystemID)
 	if err != nil {
 		return nil, err
@@ -246,15 +246,15 @@ func getEnvsFromExternalSystem(setting *commonmodels.ExternalSetting) ([]map[str
 		return nil, errors.New("failed to get response from external system")
 	}
 	decoder := json.NewDecoder(resp.Body)
-	respList := make([]map[string]interface{}, 0)
+	respList := make([]map[string]string, 0)
 	err = decoder.Decode(&respList)
 	if err != nil {
 		return nil, err
 	}
-	envList := make([]map[string]interface{}, 0)
+	envList := make([]map[string]string, 0)
 	// find every single key required
 	for _, respItem := range respList {
-		item := map[string]interface{}{}
+		item := map[string]string{}
 		for _, kv := range setting.Params {
 			item[kv.ParamKey] = respItem[kv.ResponseKey]
 		}
