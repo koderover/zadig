e19289785c2433c1ba374729e95b92df8cc43d11
liu deyi <andrew@koderover.com>
2021-11-29 21:52:27 +0800

    debug code
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/reaper/core/service/reaper/artifact.go
+++ pkg/microservice/reaper/core/service/reaper/artifact.go
@@ -115,6 +115,8 @@ func artifactsUpload(ctx *meta.Context, activeWorkspace string, artifactPaths []
 		st, _ := os.Stat(artifactPath)
 		if store != nil {
 			objectKey := store.GetObjectPath(artifactPath)
+			log.Infof("st.Name():%+v", st.Name())
+			log.Infof("objectKey:%s", objectKey)
 			if err = s3FileUpload(store, st.Name(), objectKey); err != nil {
 				return err
 			}
