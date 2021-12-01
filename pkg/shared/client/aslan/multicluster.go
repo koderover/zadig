8112c9c0d3fbd4f5c6b6e2f0c86d9795e2c4fab1
liu deyi <andrew@koderover.com>
2021-12-01 17:28:07 +0800

    modify local cluster status
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/shared/client/aslan/multicluster.go
+++ pkg/shared/client/aslan/multicluster.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/koderover/zadig/pkg/setting"
+
 	"github.com/koderover/zadig/pkg/tool/httpclient"
 )
 
 type cluster struct {
-	Name       string `json:"name"`
-	Production bool   `json:"production"`
-	Local      bool   `json:"local"`
+	Name   string                   `json:"name"`
+	Status setting.K8SClusterStatus `json:"status"`
+	Local  bool                     `json:"local"`
 }
 
 func (c *Client) AddMultiCluster() error {
 	timeStamp := time.Now().Format("20060102150405")
 	url := "/cluster/clusters"
 	req := cluster{
-		Name:  fmt.Sprintf("%s-%s", "local", timeStamp),
-		Local: true,
+		Name:   fmt.Sprintf("%s-%s", "local", timeStamp),
+		Local:  true,
+		Status: setting.Normal,
 	}
 
 	_, err := c.Post(url, httpclient.SetBody(req))
