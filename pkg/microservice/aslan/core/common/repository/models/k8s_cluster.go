437568ce43d089bc5bef7abb253c6578cc99757a
lou <alex1988@outlook.com>
2021-12-01 14:59:02 +0800

    update after review
    Signed-off-by: lou <alex1988@outlook.com>

--- pkg/microservice/aslan/core/common/repository/models/k8s_cluster.go
+++ pkg/microservice/aslan/core/common/repository/models/k8s_cluster.go
@@ -29,20 +29,20 @@ import (
 var namePattern = regexp.MustCompile(`^[0-9a-zA-Z_.-]{1,32}$`)
 
 type K8SCluster struct {
-	ID           primitive.ObjectID       `json:"id,omitempty"                bson:"_id,omitempty"`
-	Name         string                   `json:"name" bson:"name"`
-	Tags         []string                 `json:"tags" bson:"tags"`
-	Description  string                   `json:"description" bson:"description"`
-	Namespace    string                   `json:"namespace" bson:"namespace"`
+	ID           primitive.ObjectID       `json:"id,omitempty"   bson:"_id,omitempty"`
+	Name         string                   `json:"name"           bson:"name"`
+	Tags         []string                 `json:"tags"           bson:"tags"`
+	Description  string                   `json:"description"    bson:"description"`
+	Namespace    string                   `json:"namespace"      bson:"namespace"`
 	Info         *K8SClusterInfo          `json:"info,omitempty" bson:"info,omitempty"`
-	Status       setting.K8SClusterStatus `json:"status" bson:"status"`
-	Error        string                   `json:"error" bson:"error"`
-	Yaml         string                   `json:"yaml" bson:"yaml"`
-	Production   bool                     `json:"production" bson:"production"`
-	CreatedAt    int64                    `json:"createdAt" bson:"createdAt"`
-	CreatedBy    string                   `json:"createdBy" bson:"createdBy"`
-	Disconnected bool                     `json:"-" bson:"disconnected"`
-	Token        string                   `json:"token" bson:"-"`
+	Status       setting.K8SClusterStatus `json:"status"         bson:"status"`
+	Error        string                   `json:"error"          bson:"error"`
+	Yaml         string                   `json:"yaml"           bson:"yaml"`
+	Production   bool                     `json:"production"     bson:"production"`
+	CreatedAt    int64                    `json:"createdAt"      bson:"createdAt"`
+	CreatedBy    string                   `json:"createdBy"      bson:"createdBy"`
+	Disconnected bool                     `json:"-"              bson:"disconnected"`
+	Token        string                   `json:"token"          bson:"-"`
 	Provider     int8                     `json:"provider"       bson:"provider"`
 }
 
