3c7cf7701aadf3ff1d7e622b46028f2402c0820c
huangzexiong <huangzexiong@52tt.com>
2021-12-08 18:46:56 +0800

    test add $SERVICES params
    Signed-off-by: huangzexiong <huangzexiong@52tt.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/workflow_task.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/workflow_task.go
@@ -1158,6 +1158,8 @@ func workFlowArgsToTaskArgs(target string, workflowArgs *commonmodels.WorkflowTa
 // TODO 和validation中转化testsubtask合并为一个方法
 func testArgsToSubtask(args *commonmodels.WorkflowTaskArgs, pt *task.Task, log *zap.SugaredLogger) ([]*task.Testing, error) {
 	var resp []*task.Testing
+	var servicesArray []string
+	var services string
 
 	// 创建任务的测试参数为脱敏数据，需要转换为实际数据
 	for _, test := range args.Tests {
@@ -1165,6 +1167,15 @@ func testArgsToSubtask(args *commonmodels.WorkflowTaskArgs, pt *task.Task, log *
 		if err == nil && existed.PreTest != nil {
 			commonservice.EnsureSecretEnvs(existed.PreTest.Envs, test.Envs)
 		}
+	}
+
+	// 获取本次任务执行的服务，按照逗号分隔,service1,service2...
+	if len(args.Target) > 0 {
+		for _, service := range args.Target {
+			servicesArray = append(servicesArray, service.ServiceName)
+		}
+		services = strings.Replace(strings.Trim(fmt.Sprint(servicesArray), "[]"), " ", ",", -1)
+		log.Info("获取本次任务执行的服务services:%v", services)
 	}
 
 	testArgs := args.Tests
@@ -1224,6 +1235,9 @@ func testArgsToSubtask(args *commonmodels.WorkflowTaskArgs, pt *task.Task, log *
 				}
 			}
 			envs = append(envs, &commonmodels.KeyVal{Key: "TEST_URL", Value: GetLink(pt, configbase.SystemAddress(), config.WorkflowType)})
+			envs = append(envs, &commonmodels.KeyVal{Key: "SERVICES", Value: services})
+			log.Info("test envs:%v", envs)
+
 			testTask.JobCtx.EnvVars = envs
 			testTask.ImageID = testModule.PreTest.ImageID
 			testTask.BuildOS = testModule.PreTest.BuildOS
