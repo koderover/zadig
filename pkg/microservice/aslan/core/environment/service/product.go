d35d2210491bd38ecdf5f55df6bee0e258e9715c
liu deyi <andrew@koderover.com>
2021-12-08 18:57:08 +0800

    optimize code
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/aslan/core/environment/service/product.go
+++ pkg/microservice/aslan/core/environment/service/product.go
@@ -209,7 +209,7 @@ func buildProductResp(envName string, prod *commonmodels.Product, log *zap.Sugar
 		prodResp.ClusterName = cluster.Name
 		prodResp.IsLocal = cluster.Local
 
-		if !clusterService.ClusterConnected(prod.ClusterID) {
+		if !prodResp.IsLocal && !clusterService.ClusterConnected(prod.ClusterID) {
 			prodResp.Status = setting.ClusterDisconnected
 			prodResp.Error = "集群未连接"
 			return prodResp
