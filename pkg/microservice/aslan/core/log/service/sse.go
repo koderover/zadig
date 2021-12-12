36c83de6634cc0cb4523c553444a7edaf3c999f3
liu deyi <andrew@koderover.com>
2021-12-12 12:32:26 +0800

    fix sse log bug
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/aslan/core/log/service/sse.go
+++ pkg/microservice/aslan/core/log/service/sse.go
@@ -139,7 +139,7 @@ func TaskContainerLogStream(ctx context.Context, streamChan chan interface{}, op
 				Targets: []string{options.ServiceName},
 			})
 		}
-		if build != nil && build.PreBuild != nil {
+		if build != nil && build.PreBuild != nil && build.PreBuild.ClusterID != "" && build.PreBuild.Namespace != "" {
 			options.ClusterID = build.PreBuild.ClusterID
 			options.Namespace = build.PreBuild.Namespace
 		}
@@ -157,7 +157,7 @@ func TestJobContainerLogStream(ctx context.Context, streamChan chan interface{},
 	selector := getPipelineSelector(options)
 	// get cluster ID
 	testing, _ := commonrepo.NewTestingColl().Find(getTestName(options.ServiceName), "")
-	if testing != nil && testing.PreTest != nil {
+	if testing != nil && testing.PreTest != nil && testing.PreTest.ClusterID != "" && testing.PreTest.Namespace != "" {
 		options.ClusterID = testing.PreTest.ClusterID
 		options.Namespace = testing.PreTest.Namespace
 	}
