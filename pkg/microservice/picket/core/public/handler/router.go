c835ba889dbac46970bf13affa018963d10d1ea6
mouuii <chaoyueshijian@gmail.com>
2021-12-14 18:24:26 +0800

    workflowtask
    Signed-off-by: mouuii <chaoyueshijian@gmail.com>

--- pkg/microservice/picket/core/public/handler/router.go
+++ pkg/microservice/picket/core/public/handler/router.go
@@ -27,7 +27,7 @@ func (*Router) Inject(router *gin.RouterGroup) {
 	dev := router.Group("")
 	{
 		dev.POST("/workflowTask/create", CreateWorkflowTask)
-		dev.DELETE("workflowTask/id/:id/pipelines/:name/cancel", CancelWorkflowTask)
+		dev.POST("workflowTask/id/:id/pipelines/:name/cancel", CancelWorkflowTask)
 		dev.POST("/workflowTask/id/:id/pipelines/:name/restart", RestartWorkflowTask)
 		dev.GET("/workflowTask", ListWorkflowTask)
 		dev.GET("/dc/releases", ListDelivery)
