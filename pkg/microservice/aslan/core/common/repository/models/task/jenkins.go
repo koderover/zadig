b4275b3838f9dd3b15cd0c7f2bc85a7785d0c0e3
liu deyi <andrew@koderover.com>
2021-12-28 11:47:37 +0800

    field add omitempty attribute
    Signed-off-by: liu deyi <andrew@koderover.com>

--- pkg/microservice/aslan/core/common/repository/models/task/jenkins.go
+++ pkg/microservice/aslan/core/common/repository/models/task/jenkins.go
@@ -48,15 +48,15 @@ type JenkinsBuild struct {
 	ServiceName        string                      `bson:"service_name"            json:"service_name"`
 	Service            string                      `bson:"service"                 json:"service"`
 	OnSetup            string                      `bson:"setup,omitempty"         json:"setup,omitempty"`
-	Timeout            int                         `bson:"timeout"                 json:"timeout,omitempty"`
-	Error              string                      `bson:"error"                   json:"error,omitempty"`
+	Timeout            int                         `bson:"timeout,omitempty"       json:"timeout,omitempty"`
+	Error              string                      `bson:"error,omitempty"         json:"error,omitempty"`
 	ResReq             setting.Request             `bson:"res_req"                 json:"res_req"`
 	ResReqSpec         setting.RequestSpec         `bson:"res_req_spec"            json:"res_req_spec"`
 	JenkinsIntegration *JenkinsIntegration         `bson:"-"                       json:"jenkins_integration"`
 	JenkinsBuildArgs   *JenkinsBuildArgs           `bson:"-"                       json:"jenkins_build_args"`
 	Registries         []*models.RegistryNamespace `bson:"registries"              json:"registries"`
-	StartTime          int64                       `bson:"start_time"              json:"start_time,omitempty"`
-	EndTime            int64                       `bson:"end_time"                json:"end_time,omitempty"`
+	StartTime          int64                       `bson:"start_time,omitempty"    json:"start_time,omitempty"`
+	EndTime            int64                       `bson:"end_time,omitempty"      json:"end_time,omitempty"`
 	LogFile            string                      `bson:"log_file"                json:"log_file"`
 	Image              string                      `bson:"image,omitempty"         json:"image,omitempty"`
 	IsRestart          bool                        `bson:"is_restart"              json:"is_restart"`
