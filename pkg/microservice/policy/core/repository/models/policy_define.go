a73e03f1b632ff7944ec0030a7b12a59f02315cb
mouuii <zhouchengbin@koderover.com>
2022-01-13 18:52:51 +0800

    policy
    Signed-off-by: mouuii <zhouchengbin@koderover.com>

--- pkg/microservice/policy/core/repository/models/policy_define.go
+++ pkg/microservice/policy/core/repository/models/policy_define.go
@@ -8,16 +8,15 @@ const (
 )
 
 type PolicyDefine struct {
-	Name            string      `bson:"name"                 json:"name"`
-	Namespace       string      `bson:"namespace"            json:"namespace"`
-	Alias           string      `bson:"alias"                json:"alias"`
-	BindUserIDs     []string    `bson:"bind_user_ids"        json:"bind_user_ids"`
-	BindUserGroups  []string    `bson:"bind_user_groups"     json:"bind_user_groups"`
-	MatchAttributes []Attribute `bson:"match_attributes"     json:"match_attributes"`
-	Rules           []Rule      `bson:"rules"                json:"rules"`
-	PolicyType      PolicyType  `bson:"policy_type"          json:"policy_type"`
-	CreateTime      int64       `bson:"create_time"          json:"create_time"`
-	CreateBy        string      `bson:"create_by"            json:"create_by"`
+	Name           string     `bson:"name"                 json:"name"`
+	Namespace      string     `bson:"namespace"            json:"namespace"`
+	Alias          string     `bson:"alias"                json:"alias"`
+	BindUserIDs    []string   `bson:"bind_user_ids"        json:"bind_user_ids"`
+	BindUserGroups []string   `bson:"bind_user_groups"     json:"bind_user_groups"`
+	Rules          []Rule     `bson:"rules"                json:"rules"`
+	PolicyType     PolicyType `bson:"policy_type"          json:"policy_type"`
+	CreateTime     int64      `bson:"create_time"          json:"create_time"`
+	CreateBy       string     `bson:"create_by"            json:"create_by"`
 }
 
 func (PolicyDefine) TableName() string {
