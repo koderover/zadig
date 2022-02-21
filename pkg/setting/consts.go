a5b179f97f6f4ed4d947fca1b68f7f249190e31b
zhangyifei <zhangyifei@koderover.com>
2022-02-21 17:18:44 +0800

    fix bug of scheduling build/test tasks
    Signed-off-by: zhangyifei <zhangyifei@koderover.com>

--- pkg/setting/consts.go
+++ pkg/setting/consts.go
@@ -612,3 +612,7 @@ const (
 const (
 	InformerNamingConvention = "%s-%s"
 )
+
+// AttachedClusterNamespace is the namespace Zadig uses in attached cluster.
+// Note: **Restricted because of product design since v1.9.0**.
+const AttachedClusterNamespace = "koderover-agent"
