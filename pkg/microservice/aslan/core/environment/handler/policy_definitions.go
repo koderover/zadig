c23d6d65ddb5095729decb51beb1806a129fb41a
panxunying <panxunying@koderover.com>
2022-02-23 14:19:11 +0800

    update policy yaml
    Signed-off-by: panxunying <panxunying@koderover.com>

--- pkg/microservice/aslan/core/environment/handler/policy_definitions.go
+++ pkg/microservice/aslan/core/environment/handler/policy_definitions.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	productionPolicyResource = "ProductionEnvironment"
-	productionPolicyAlias    = "集成环境(类生产/预发布)"
+	productionPolicyAlias    = "集成环境(生产/预发布)"
 	productionKey            = "production"
 	productionValueTrue      = "true"
 )
