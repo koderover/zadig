1b2100a64ca003a04450bfb89d8f68195ff8bedb
mouuii <zhouchengbin@koderover.com>
2022-05-07 11:22:56 +0800

    move workflow to all
    Signed-off-by: mouuii <zhouchengbin@koderover.com>

--- pkg/microservice/aslan/core/meta/meta.go
+++ pkg/microservice/aslan/core/meta/meta.go
@@ -13,46 +13,11 @@ import (
 //go:embed env_meta.yaml
 var envMetaBytes []byte
 
-//go:embed workflow_meta.yaml
-var workflowPolicyMeta []byte
-
 //go:embed other_metas.yaml
 var otherPolicyMeta []byte
 
 type MetaGetter interface {
 	Policies() []*policy.PolicyMeta
-}
-
-type MetaWorkflow struct {
-}
-
-func (m *MetaWorkflow) Policies() []*policy.PolicyMeta {
-	res := &policy.PolicyMeta{}
-	err := yaml.Unmarshal(workflowPolicyMeta, res)
-	if err != nil {
-		// should not have happened here
-		log.DPanic(err)
-	}
-	for i, meta := range res.Rules {
-		tmpRules := []*policy.ActionRule{}
-		for _, rule := range meta.Rules {
-			if rule.ResourceType == "" {
-				rule.ResourceType = res.Resource
-			}
-			if strings.Contains(rule.Endpoint, ":name") {
-				idRegex := strings.ReplaceAll(rule.Endpoint, ":name", `([\w\W].*)`)
-				idRegex = strings.ReplaceAll(idRegex, "?*", `[\w\W].*`)
-				endpoint := strings.ReplaceAll(rule.Endpoint, ":name", "?*")
-				rule.Endpoint = endpoint
-				rule.IDRegex = idRegex
-			}
-			tmpRules = append(tmpRules, rule)
-		}
-		res.Rules[i].Rules = tmpRules
-	}
-	return []*policy.PolicyMeta{
-		res,
-	}
 }
 
 type MetaEnvironment struct {
@@ -135,6 +100,27 @@ func (m *MetaOthers) Policies() []*policy.PolicyMeta {
 	if err := yaml.Unmarshal(otherPolicyMeta, &otherMetas); err != nil {
 		log.DPanic(err)
 	}
+	for _, meta := range otherMetas {
+		if meta.Resource == "Workflow" {
+			for _, ruleMeta := range meta.Rules {
+				tmpRules := []*policy.ActionRule{}
+				for _, rule := range ruleMeta.Rules {
+					if rule.ResourceType == "" {
+						rule.ResourceType = "Workflow"
+					}
+					if strings.Contains(rule.Endpoint, ":name") {
+						idRegex := strings.ReplaceAll(rule.Endpoint, ":name", `([\w\W].*)`)
+						idRegex = strings.ReplaceAll(idRegex, "?*", `[\w\W].*`)
+						endpoint := strings.ReplaceAll(rule.Endpoint, ":name", "?*")
+						rule.Endpoint = endpoint
+						rule.IDRegex = idRegex
+					}
+					tmpRules = append(tmpRules, rule)
+				}
+				ruleMeta.Rules = tmpRules
+			}
+		}
+	}
 	return otherMetas
 }
 
@@ -152,6 +138,6 @@ func (m *MetaAll) Policies() []*policy.PolicyMeta {
 
 func NewMetaGetter() MetaGetter {
 	all := &MetaAll{}
-	all.metaGetters = append(all.metaGetters, new(MetaOthers), new(MetaSystem), new(MetaWorkflow), new(MetaEnvironment))
+	all.metaGetters = append(all.metaGetters, new(MetaOthers), new(MetaEnvironment))
 	return all
 }
