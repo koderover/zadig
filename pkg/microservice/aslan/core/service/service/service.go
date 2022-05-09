a2a104b930281bb5a62bf4a34f947244ab308d2a
ddh27 <duandehua@koderover.com>
2022-05-09 17:29:48 +0800

    Optimized YAML verification error message
    Signed-off-by: ddh27 <duandehua@koderover.com>

--- pkg/microservice/aslan/core/service/service/service.go
+++ pkg/microservice/aslan/core/service/service/service.go
@@ -964,8 +964,7 @@ func YamlValidator(args *YamlValidatorReq) []string {
 			yamlData = config.ServiceNameAlias.ReplaceAllLiteralString(yamlData, args.ServiceName)
 
 			if err := yaml.Unmarshal([]byte(yamlData), &resKind); err != nil {
-				log.Errorf("yaml unmarshal err: %s", err)
-				errorDetails = append(errorDetails, "Invalid yaml format. The content must be a series of valid Kubernetes resources")
+				errorDetails = append(errorDetails, fmt.Sprintf("Invalid yaml format. The content must be a series of valid Kubernetes resources. err: %s", err))
 			}
 		}
 	}
