334cd8991789d23962e3c7e2c34b61f1da21eee3
ddh27 <duandehua@koderover.com>
2022-05-31 15:20:05 +0800

    Clusters can be connected to HubAgent
    Signed-off-by: ddh27 <duandehua@koderover.com>

--- pkg/microservice/aslan/core/service.go
+++ pkg/microservice/aslan/core/service.go
@@ -36,6 +36,7 @@ import (
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/webhook"
 	environmentservice "github.com/koderover/zadig/pkg/microservice/aslan/core/environment/service"
 	labelMongodb "github.com/koderover/zadig/pkg/microservice/aslan/core/label/repository/mongodb"
+	multiclusterservice "github.com/koderover/zadig/pkg/microservice/aslan/core/multicluster/service"
 	systemrepo "github.com/koderover/zadig/pkg/microservice/aslan/core/system/repository/mongodb"
 	systemservice "github.com/koderover/zadig/pkg/microservice/aslan/core/system/service"
 	workflowservice "github.com/koderover/zadig/pkg/microservice/aslan/core/workflow/service/workflow"
@@ -131,6 +132,8 @@ func Start(ctx context.Context) {
 	go environmentservice.StartClusterInformer()
 
 	go StartControllers(ctx.Done())
+
+	go multiclusterservice.ClusterApplyUpgradeAgent()
 
 	initRsaKey()
 }
