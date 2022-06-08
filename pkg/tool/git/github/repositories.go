a3b1e43da38eeb89da61a8738542c564e9491fd5
shendongdong <shendongdong@koderover.com>
2022-06-08 18:30:03 +0800

    cherry-pick 1658 (#1659)
    * fix github list project not found bug
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * fix github fetch project bug
    
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/tool/git/github/repositories.go
+++ pkg/tool/git/github/repositories.go
@@ -31,7 +31,8 @@ import (
 
 func (c *Client) ListRepositoriesForAuthenticatedUser(ctx context.Context, user string, opts *ListOptions) ([]*github.Repository, error) {
 	repositories, err := wrap(paginated(func(o *github.ListOptions) ([]interface{}, *github.Response, error) {
-		rs, r, err := c.Repositories.List(ctx, user, &github.RepositoryListOptions{ListOptions: *o})
+		// Note. parameter user is not used when list repositories because private repos will NOT be in response data from gitHub even user is the exact owner of these private repos
+		rs, r, err := c.Repositories.List(ctx, "", &github.RepositoryListOptions{ListOptions: *o})
 		var res []interface{}
 		for _, r := range rs {
 			res = append(res, r)
