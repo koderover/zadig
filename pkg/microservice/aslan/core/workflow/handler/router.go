c4778d26fb878842f31879d0f3afe338ba1edc1b
guoyu <guoyu@koderover.com>
2022-08-15 15:33:36 +0800

    move api path
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/workflow/handler/router.go
+++ pkg/microservice/aslan/core/workflow/handler/router.go
@@ -175,6 +175,11 @@ func (*Router) Inject(router *gin.RouterGroup) {
 		workflowV4.PUT("/:name", UpdateWorkflowV4)
 		workflowV4.DELETE("/:name", DeleteWorkflowV4)
 		workflowV4.GET("/preset/:name", GetWorkflowV4Preset)
+		workflowV4.GET("/webhook/preset", GetWebhookForWorkflowV4Preset)
+		workflowV4.GET("/webhook", ListWebhookForWorkflowV4Preset)
+		workflowV4.POST("/webhook/:workflowName", CreateWebhookForWorkflowV4)
+		workflowV4.PUT("/webhook/:workflowName", UpdateWebhookForWorkflowV4)
+		workflowV4.DELETE("/webhook/:workflowName/trigger/:triggerName", DeleteWebhookForWorkflowV4)
 	}
 
 	// ---------------------------------------------------------------------------------------
@@ -188,11 +193,6 @@ func (*Router) Inject(router *gin.RouterGroup) {
 		taskV4.DELETE("/workflow/:workflowName/task/:taskID", CancelWorkflowTaskV4)
 		taskV4.GET("/clone/workflow/:workflowName/task/:taskID", CloneWorkflowTaskV4)
 		taskV4.POST("/approve", ApproveStage)
-		taskV4.GET("/webhook/preset", GetWebhookForWorkflowV4Preset)
-		taskV4.GET("/webhook", ListWebhookForWorkflowV4Preset)
-		taskV4.POST("/webhook/:workflowName", CreateWebhookForWorkflowV4)
-		taskV4.PUT("/webhook/:workflowName", UpdateWebhookForWorkflowV4)
-		taskV4.DELETE("/webhook/:workflowName/trigger/:triggerName", DeleteWebhookForWorkflowV4)
 	}
 
 	// ---------------------------------------------------------------------------------------
