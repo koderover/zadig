25e504265186d9751f98bddad8f1fd586273c5d2
shendongdong <shendongdong@koderover.com>
2022-08-18 11:28:22 +0800

    fix scanning job sse log error (#1905)
    * fix scanning job sse log error
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * fix scanning job sse log error
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/log/handler/sse.go
+++ pkg/microservice/aslan/core/log/handler/sse.go
@@ -27,6 +27,7 @@ import (
 	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	logservice "github.com/koderover/zadig/pkg/microservice/aslan/core/log/service"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/workflow/testing/service"
+	"github.com/koderover/zadig/pkg/setting"
 	internalhandler "github.com/koderover/zadig/pkg/shared/handler"
 	e "github.com/koderover/zadig/pkg/tool/errors"
 	"github.com/koderover/zadig/pkg/util/ginzap"
@@ -311,15 +312,26 @@ func GetScanningContainerLogsSSE(c *gin.Context) {
 	}
 
 	resp, err := service.GetScanningModuleByID(id, ctx.Logger)
+
+	clusterId := ""
+	namespace := config.Namespace()
+	if resp.AdvancedSetting != nil {
+		clusterId = resp.AdvancedSetting.ClusterID
+	}
+	if clusterId != "" && clusterId != setting.LocalClusterID {
+		namespace = setting.AttachedClusterNamespace
+	}
+
 	scanningName := fmt.Sprintf("%s-%s-%s", resp.Name, id, "scanning-job")
 	options := &logservice.GetContainerOptions{
-		Namespace:    config.Namespace(),
+		Namespace:    namespace,
 		PipelineName: scanningName,
 		SubTask:      string(config.TaskScanning),
 		TailLines:    tails,
 		TaskID:       taskID,
 		PipelineType: string(config.ScanningType),
 		ServiceName:  resp.Name,
+		ClusterID:    clusterId,
 	}
 
 	internalhandler.Stream(c, func(ctx1 context.Context, streamChan chan interface{}) {
