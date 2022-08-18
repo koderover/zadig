830ea54eff75f1939327785bd6ec4a4c12a2e556
dianqihanwangzi <dianqihanwangzi@gmail.com>
2022-08-18 22:01:43 +0800

    Cherry pick 1921 (#1922)
    * save origin cunstom workflow args for task clone
    
    Signed-off-by: guoyu <guoyu@koderover.com>
    
    * fix error msg
    
    Signed-off-by: guoyu <guoyu@koderover.com>
    
    Signed-off-by: guoyu <guoyu@koderover.com>
    Co-authored-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/common/repository/models/wokflow_task_v4.go
+++ pkg/microservice/aslan/core/common/repository/models/wokflow_task_v4.go
@@ -23,26 +23,27 @@ import (
 )
 
 type WorkflowTask struct {
-	ID            primitive.ObjectID `bson:"_id,omitempty"             json:"id,omitempty"`
-	TaskID        int64              `bson:"task_id"                   json:"task_id"`
-	WorkflowName  string             `bson:"workflow_name"             json:"workflow_name"`
-	Params        []*Param           `bson:"params"                    json:"params"`
-	WorkflowArgs  *WorkflowV4        `bson:"workflow_args"             json:"workflow_args"`
-	KeyVals       []*KeyVal          `bson:"key_vals"                  json:"key_vals"`
-	GlobalContext map[string]string  `bson:"global_context"            json:"global_context"`
-	Status        config.Status      `bson:"status"                    json:"status,omitempty"`
-	TaskCreator   string             `bson:"task_creator"              json:"task_creator,omitempty"`
-	TaskRevoker   string             `bson:"task_revoker,omitempty"    json:"task_revoker,omitempty"`
-	CreateTime    int64              `bson:"create_time"               json:"create_time,omitempty"`
-	StartTime     int64              `bson:"start_time"                json:"start_time,omitempty"`
-	EndTime       int64              `bson:"end_time"                  json:"end_time,omitempty"`
-	Stages        []*StageTask       `bson:"stages"                    json:"stages"`
-	ProjectName   string             `bson:"project_name,omitempty"    json:"project_name,omitempty"`
-	IsDeleted     bool               `bson:"is_deleted"                json:"is_deleted"`
-	IsArchived    bool               `bson:"is_archived"               json:"is_archived"`
-	Error         string             `bson:"error,omitempty"           json:"error,omitempty"`
-	IsRestart     bool               `bson:"is_restart"                json:"is_restart"`
-	MultiRun      bool               `bson:"multi_run"                 json:"multi_run"`
+	ID                 primitive.ObjectID `bson:"_id,omitempty"             json:"id,omitempty"`
+	TaskID             int64              `bson:"task_id"                   json:"task_id"`
+	WorkflowName       string             `bson:"workflow_name"             json:"workflow_name"`
+	Params             []*Param           `bson:"params"                    json:"params"`
+	WorkflowArgs       *WorkflowV4        `bson:"workflow_args"             json:"workflow_args"`
+	OriginWorkflowArgs *WorkflowV4        `bson:"origin_workflow_args"      json:"origin_workflow_args"`
+	KeyVals            []*KeyVal          `bson:"key_vals"                  json:"key_vals"`
+	GlobalContext      map[string]string  `bson:"global_context"            json:"global_context"`
+	Status             config.Status      `bson:"status"                    json:"status,omitempty"`
+	TaskCreator        string             `bson:"task_creator"              json:"task_creator,omitempty"`
+	TaskRevoker        string             `bson:"task_revoker,omitempty"    json:"task_revoker,omitempty"`
+	CreateTime         int64              `bson:"create_time"               json:"create_time,omitempty"`
+	StartTime          int64              `bson:"start_time"                json:"start_time,omitempty"`
+	EndTime            int64              `bson:"end_time"                  json:"end_time,omitempty"`
+	Stages             []*StageTask       `bson:"stages"                    json:"stages"`
+	ProjectName        string             `bson:"project_name,omitempty"    json:"project_name,omitempty"`
+	IsDeleted          bool               `bson:"is_deleted"                json:"is_deleted"`
+	IsArchived         bool               `bson:"is_archived"               json:"is_archived"`
+	Error              string             `bson:"error,omitempty"           json:"error,omitempty"`
+	IsRestart          bool               `bson:"is_restart"                json:"is_restart"`
+	MultiRun           bool               `bson:"multi_run"                 json:"multi_run"`
 }
 
 func (WorkflowTask) TableName() string {
