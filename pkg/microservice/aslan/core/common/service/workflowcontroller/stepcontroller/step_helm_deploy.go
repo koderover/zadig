db7fb2ea8098d9645248cdaac595558cae0bc441
guoyu <guoyu@koderover.com>
2022-08-19 12:29:37 +0800

    fix helm deploy do not rerurn error
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/stepcontroller/step_helm_deploy.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/stepcontroller/step_helm_deploy.go
@@ -377,7 +377,7 @@ func (s *helmDeployCtl) run(ctx context.Context) error {
 		err = fmt.Errorf("failed to upgrade relase: %s, timeout", chartSpec.ReleaseName)
 	}
 	if err != nil {
-		return nil
+		return err
 	}
 
 	//替换环境变量中的chartInfos
