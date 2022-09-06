e5be14776c57034855211d0517b3c5b7bdf2d00f
guoyu <guoyu@koderover.com>
2022-09-06 11:25:32 +0800

    fix import issue
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/stepcontroller/step_docker_build.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/stepcontroller/step_docker_build.go
@@ -23,7 +23,6 @@ import (
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 
-	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	commonmodels "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
 	"github.com/koderover/zadig/pkg/types/step"
