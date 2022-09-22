af2b545440716582c32b5bf442236e9df7fe4666
guoyu <guoyu@koderover.com>
2022-09-22 17:23:21 +0800

    fix key value names
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/workflow/testing/service/testing.go
+++ pkg/microservice/aslan/core/workflow/testing/service/testing.go
@@ -148,7 +148,7 @@ type TestingOpt struct {
 	Workflows   []*commonmodels.Workflow   `bson:"-"                      json:"workflows,omitempty"`
 	Schedules   *commonmodels.ScheduleCtrl `bson:"-"                      json:"schedules,omitempty"`
 	Repos       []*types.Repository        `bson:"repos"                  json:"repos"`
-	Envs        []*commonmodels.KeyVal     `bson:"envs"                   json:"envs"`
+	KeyVal      []*commonmodels.KeyVal     `bson:"key_vals"               json:"key_vals"`
 }
 
 func ListTestingOpt(productNames []string, testType string, log *zap.SugaredLogger) ([]*TestingOpt, error) {
