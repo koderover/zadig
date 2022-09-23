cbeeb076a9834e7397dcdfc794583c4cdd1a31c9
guoyu <guoyu@koderover.com>
2022-09-23 11:37:53 +0800

    fix import issue
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/instantmessage/workflow_task.go
+++ pkg/microservice/aslan/core/common/service/instantmessage/workflow_task.go
@@ -25,13 +25,14 @@ import (
 	"text/template"
 	"time"
 
+	"k8s.io/apimachinery/pkg/util/sets"
+
 	configbase "github.com/koderover/zadig/pkg/config"
 	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
 	"github.com/koderover/zadig/pkg/tool/log"
 	"github.com/koderover/zadig/pkg/types"
 	"github.com/koderover/zadig/pkg/types/step"
-	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func (w *Service) SendWorkflowTaskAproveNotifications(workflowName string, taskID int64) error {
