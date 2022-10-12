9a3d761303ee5e4aec78082bf0fd7d576868e37d
guoyu <guoyu@koderover.com>
2022-10-12 21:01:13 +0800

    add ua
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/cli/upgradeassistant/cmd/migrate/1160.go
+++ pkg/cli/upgradeassistant/cmd/migrate/1160.go
@@ -20,8 +20,13 @@ import (
 	"context"
 
 	"github.com/koderover/zadig/pkg/cli/upgradeassistant/internal/upgradepath"
+	"github.com/koderover/zadig/pkg/microservice/aslan/config"
+	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
+	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models/task"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
 	"github.com/koderover/zadig/pkg/tool/log"
+	"github.com/koderover/zadig/pkg/types"
+	steptypes "github.com/koderover/zadig/pkg/types/step"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -54,15 +59,20 @@ func addDisplayNameToWorkflowV4() error {
 	}
 	var ms []mongo.WriteModel
 	for _, workflow := range workflows {
-		if workflow.DisplayName != "" {
-			continue
+		displayName := workflow.DisplayName
+		if workflow.DisplayName == "" {
+			displayName = workflow.Name
+		}
+		for _, webhook := range workflow.HookCtls {
+			setPRsforWorkflowV4(webhook.WorkflowArg)
 		}
 		ms = append(ms,
 			mongo.NewUpdateOneModel().
 				SetFilter(bson.D{{"_id", workflow.ID}}).
 				SetUpdate(bson.D{{"$set",
 					bson.D{
-						{"display_name", workflow.Name},
+						{"display_name", displayName},
+						{"hook_ctl", workflow.HookCtls},
 					}},
 				}),
 		)
@@ -79,15 +89,18 @@ func addDisplayNameToWorkflowV4() error {
 	}
 	var mTasks []mongo.WriteModel
 	for _, workflowTask := range workflowtasks {
-		if workflowTask.WorkflowDisplayName != "" {
-			continue
+		displayName := workflowTask.WorkflowDisplayName
+		if workflowTask.WorkflowDisplayName == "" {
+			displayName = workflowTask.WorkflowName
 		}
+		setPRsforWorkflowV4(workflowTask.OriginWorkflowArgs)
 		mTasks = append(mTasks,
 			mongo.NewUpdateOneModel().
 				SetFilter(bson.D{{"_id", workflowTask.ID}}).
 				SetUpdate(bson.D{{"$set",
 					bson.D{
-						{"workflow_display_name", workflowTask.WorkflowName},
+						{"workflow_display_name", displayName},
+						{"origin_workflow_args", workflowTask.OriginWorkflowArgs},
 					}},
 				}),
 		)
@@ -99,6 +112,59 @@ func addDisplayNameToWorkflowV4() error {
 	}
 
 	return nil
+}
+
+func setPRsforWorkflowV4(workflow *models.WorkflowV4) {
+	if workflow == nil {
+		return
+	}
+	for _, stage := range workflow.Stages {
+		for _, job := range stage.Jobs {
+			switch job.JobType {
+			case config.JobZadigBuild:
+				spec := &models.ZadigBuildJobSpec{}
+				if err := models.IToi(job.Spec, spec); err != nil {
+					continue
+				}
+				for _, svc := range spec.ServiceAndBuilds {
+					for _, repo := range svc.Repos {
+						setPRs(repo)
+					}
+				}
+				job.Spec = spec
+			case config.JobFreestyle:
+				spec := &models.FreestyleJobSpec{}
+				if err := models.IToi(job.Spec, spec); err != nil {
+					continue
+				}
+				for _, step := range spec.Steps {
+					if step.StepType != config.StepGit {
+						continue
+					}
+					stepSpec := &steptypes.StepGitSpec{}
+					if err := models.IToi(step.Spec, stepSpec); err != nil {
+						continue
+					}
+					for _, repo := range stepSpec.Repos {
+						setPRs(repo)
+					}
+					step.Spec = stepSpec
+				}
+				job.Spec = spec
+			case config.JobZadigTesting:
+				spec := &models.ZadigTestingJobSpec{}
+				if err := models.IToi(job.Spec, spec); err != nil {
+					continue
+				}
+				for _, test := range spec.TestModules {
+					for _, repo := range test.Repos {
+						setPRs(repo)
+					}
+				}
+				job.Spec = spec
+			}
+		}
+	}
 }
 
 func addDisplayNameToWorkflow() error {
@@ -133,15 +199,18 @@ func addDisplayNameToWorkflow() error {
 	}
 	var mTasks []mongo.WriteModel
 	for _, workflowTask := range workflowtasks {
-		if workflowTask.PipelineDisplayName != "" {
-			continue
+		displayName := workflowTask.PipelineDisplayName
+		if workflowTask.PipelineDisplayName == "" {
+			displayName = workflowTask.PipelineName
 		}
+		setPRsForWorkflowTask(workflowTask)
 		mTasks = append(mTasks,
 			mongo.NewUpdateOneModel().
 				SetFilter(bson.D{{"_id", workflowTask.ID}}).
 				SetUpdate(bson.D{{"$set",
 					bson.D{
-						{"pipeline_display_name", workflowTask.PipelineName},
+						{"pipeline_display_name", displayName},
+						{"workflow_args", workflowTask.WorkflowArgs},
 					}},
 				}),
 		)
@@ -151,6 +220,33 @@ func addDisplayNameToWorkflow() error {
 			return err
 		}
 	}
-
 	return nil
+}
+
+func setPRsForWorkflowTask(t *task.Task) {
+	if t.WorkflowArgs == nil {
+		return
+	}
+	for _, target := range t.WorkflowArgs.Target {
+		if target.Build == nil {
+			continue
+		}
+		for _, repo := range target.Build.Repos {
+			setPRs(repo)
+		}
+	}
+	for _, test := range t.WorkflowArgs.Tests {
+		for _, repo := range test.Builds {
+			setPRs(repo)
+		}
+	}
+}
+
+func setPRs(repo *types.Repository) {
+	if len(repo.PRs) > 0 {
+		return
+	}
+	if repo.PR > 0 {
+		repo.PRs = []int{repo.PR}
+	}
 }
