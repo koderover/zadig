3289a56421d2453aee53ef75f8dd8dd82d54302f
guoyu <guoyu@koderover.com>
2022-10-13 11:24:39 +0800

    fix log
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/workflow_v4.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/workflow_v4.go
@@ -415,9 +415,9 @@ func LintWorkflowV4(workflow *commonmodels.WorkflowV4, logger *zap.SugaredLogger
 		return e.ErrUpsertWorkflow.AddErr(err)
 	}
 	if !match {
-		err := fmt.Errorf("workflow name should match %s", WorkflowRegx)
-		logger.Errorf(err.Error())
-		return e.ErrUpsertWorkflow.AddErr(err)
+		errMsg := "工作流标识支持小写字母、数字和中划线"
+		logger.Error(errMsg)
+		return e.ErrUpsertWorkflow.AddDesc(errMsg)
 	}
 
 	project := &template.Product{}
