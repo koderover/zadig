7a23c4ae4a56ea19cda6dd4f42fdaf32e768653d
guoyu <guoyu@koderover.com>
2022-10-18 15:38:29 +0800

    register job
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job.go
@@ -59,6 +59,8 @@ func initJobCtl(job *commonmodels.JobTask, workflowCtx *commonmodels.WorkflowTas
 		jobCtl = NewBlueGreenDeployJobCtl(job, workflowCtx, ack, logger)
 	case string(config.JobK8sBlueGreenRelease):
 		jobCtl = NewBlueGreenReleaseJobCtl(job, workflowCtx, ack, logger)
+	case string(config.JobK8sPatch):
+		jobCtl = NewK8sPatchJobCtl(job, workflowCtx, ack, logger)
 	default:
 		jobCtl = NewFreestyleJobCtl(job, workflowCtx, ack, logger)
 	}
