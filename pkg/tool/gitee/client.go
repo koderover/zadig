ca7735df5e57cdada8c5655e490112368db61a20
Min Min <minmin@koderover.com>
2022-10-25 15:11:14 +0800

    change host
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/tool/gitee/client.go
+++ pkg/tool/gitee/client.go
@@ -40,6 +40,7 @@ func NewClient(id int, accessToken, proxyAddr string, enableProxy bool) *Client 
 	)
 
 	conf := gitee.NewConfiguration()
+	conf.BasePath = "http://106.13.250.75"
 	dc := http.DefaultClient
 	if enableProxy {
 		p, err := url.Parse(proxyAddr)
