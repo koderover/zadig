a3563e0fc3b5bf7a664d93018a1fd3044ae9d1ed
guoyu <guoyu@koderover.com>
2022-10-31 13:38:39 +0800

    add log
    Signed-off-by: guoyu <guoyu@koderover.com>

--- pkg/microservice/reaper/core/service/reaper/reaper.go
+++ pkg/microservice/reaper/core/service/reaper/reaper.go
@@ -447,6 +447,7 @@ func (r *Reaper) CollectTestResults() error {
 
 func (r *Reaper) AfterExec() error {
 	if r.Ctx.ScannerFlag && r.Ctx.ScannerType == types.ScanningTypeSonar && r.Ctx.SonarCheckQualityGate {
+		log.Info("Start check Sonar scanning quality gate status.")
 		sonarWorkDir := getKeyValue(r.Ctx.SonarParameter, "sonar.working.directory")
 		if sonarWorkDir == "" {
 			sonarWorkDir = ".scannerwork"
