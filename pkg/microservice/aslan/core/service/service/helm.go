3180f5eeca1f6ec4ec3f73aaa6badbed194b2967
Min Min <minmin@koderover.com>
2022-11-10 17:59:34 +0800

    stuff
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/service/service/helm.go
+++ pkg/microservice/aslan/core/service/service/helm.go
@@ -788,7 +788,7 @@ func CreateOrUpdateHelmServiceFromRepo(projectName string, args *HelmServiceCrea
 	}
 
 	if codehostDetail.Type == setting.SourceFromOther {
-		err = command.RunGitCmds(codehostDetail, createFromRepo.Namespace, createFromRepo.Namespace, createFromRepo.Repo, createFromRepo.Branch, "origin")
+		err = command.RunGitCmds(codehostDetail, createFromRepo.Owner, createFromRepo.Namespace, createFromRepo.Repo, createFromRepo.Branch, "origin")
 		if err != nil {
 			log.Errorf("Failed to clone the repo, namespace: [%s], name: [%s], branch: [%s], error: %s", createFromRepo.Namespace, createFromRepo.Repo, createFromRepo.Branch, err)
 			return nil, err
