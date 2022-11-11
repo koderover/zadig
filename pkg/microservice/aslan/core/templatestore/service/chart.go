0765e4112ad91f12eca5de05b79177abc224a7fa
Min Min <minmin@koderover.com>
2022-11-11 19:51:25 +0800

    xd
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/templatestore/service/chart.go
+++ pkg/microservice/aslan/core/templatestore/service/chart.go
@@ -18,8 +18,6 @@ package service
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -527,7 +525,7 @@ func processChartFromGitRepo(name string, args *fs.DownloadFromSourceArgs, logge
 			err = mkdirErr
 			return
 		}
-		copyErr := copyDirectory(currentChartPath, newFilePath)
+		copyErr := copy.Copy(currentChartPath, newFilePath)
 		if copyErr != nil {
 			logger.Errorf("Failed to copy directory, err: %s", copyErr)
 			err = copyErr
@@ -585,62 +583,4 @@ func processChartFromGitRepo(name string, args *fs.DownloadFromSourceArgs, logge
 	}
 
 	return sha1, nil
-}
-
-func copyDirectory(src, dest string) error {
-	var err error
-	var fds []os.FileInfo
-	var srcinfo os.FileInfo
-
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-
-	if err = os.MkdirAll(dest, srcinfo.Mode()); err != nil {
-		return err
-	}
-
-	if fds, err = ioutil.ReadDir(src); err != nil {
-		return err
-	}
-	for _, fd := range fds {
-		srcfp := path.Join(src, fd.Name())
-		dstfp := path.Join(dest, fd.Name())
-
-		if fd.IsDir() {
-			if err = copyDirectory(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			if err = copyFile(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-	return nil
-}
-
-func copyFile(src, dst string) error {
-	var err error
-	var srcfd *os.File
-	var dstfd *os.File
-	var srcinfo os.FileInfo
-
-	if srcfd, err = os.Open(src); err != nil {
-		return err
-	}
-	defer srcfd.Close()
-
-	if dstfd, err = os.Create(dst); err != nil {
-		return err
-	}
-	defer dstfd.Close()
-
-	if _, err = io.Copy(dstfd, srcfd); err != nil {
-		return err
-	}
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-	return os.Chmod(dst, srcinfo.Mode())
 }
