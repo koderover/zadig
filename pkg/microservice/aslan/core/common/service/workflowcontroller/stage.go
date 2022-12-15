8596a142098eb1041ef52b6eccd0c0b550ed3309
M-Cosmosss <yuzhou@koderover.com>
2022-12-15 15:02:15 +0800

    feat: add description in lark approval
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/stage.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/stage.go
@@ -197,6 +197,10 @@ func waitForLarkApprove(ctx context.Context, stage *commonmodels.StageTask, work
 		workflowCtx.TaskID,
 		url.QueryEscape(workflowCtx.WorkflowDisplayName),
 	)
+	descForm := ""
+	if stage.Approval.Description != "" {
+		descForm = fmt.Sprintf("\n描述: %s", stage.Approval.Description)
+	}
 	instance, err := client.CreateApprovalInstance(&lark.CreateApprovalInstanceArgs{
 		ApprovalCode: data.LarkDefaultApprovalCode,
 		UserOpenID:   userID,
@@ -206,8 +210,8 @@ func waitForLarkApprove(ctx context.Context, stage *commonmodels.StageTask, work
 			}
 			return list
 		}(),
-		FormContent: fmt.Sprintf("项目名称: %s\n工作流名称: %s\n阶段名称: %s\n\n更多详见: %s",
-			workflowCtx.ProjectName, workflowCtx.WorkflowDisplayName, stage.Name, detailURL),
+		FormContent: fmt.Sprintf("项目名称: %s\n工作流名称: %s%s\n阶段名称: %s\n\n更多详见: %s",
+			workflowCtx.ProjectName, workflowCtx.WorkflowDisplayName, descForm, stage.Name, detailURL),
 	})
 	if err != nil {
 		log.Errorf("waitForLarkApprove: create instance failed: %v", err)
