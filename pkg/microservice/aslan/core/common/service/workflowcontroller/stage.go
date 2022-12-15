789b9bd407f360763f54aeab35612e289a2811f9
M-Cosmosss <yuzhou@koderover.com>
2022-12-15 15:09:18 +0800

    wip
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/stage.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/stage.go
@@ -210,7 +210,7 @@ func waitForLarkApprove(ctx context.Context, stage *commonmodels.StageTask, work
 			}
 			return list
 		}(),
-		FormContent: fmt.Sprintf("项目名称: %s\n工作流名称: %s%s\n阶段名称: %s\n\n更多详见: %s",
+		FormContent: fmt.Sprintf("项目名称: %s\n工作流名称: %s\n阶段名称: %s%s\n\n更多详见: %s",
 			workflowCtx.ProjectName, workflowCtx.WorkflowDisplayName, descForm, stage.Name, detailURL),
 	})
 	if err != nil {
