19ed60f1b5d7d6378b00040d6f7b83a094088329
Min Min <jamsman94@gmail.com>
2022-12-27 15:57:03 +0800

    debug logs for list workloads in dashboard
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/system/service/dashboard.go
+++ pkg/microservice/aslan/core/system/service/dashboard.go
@@ -175,6 +175,7 @@ func GetMyEnvironment(projectName, envName, username, userID string, log *zap.Su
 	}
 	serviceList := make([]*EnvService, 0)
 	_, svcList, err := service.ListWorkloadsInEnv(envName, projectName, DefaultEnvFilter, math.MaxInt, 1, log)
+	log.Infof("svcList length is: %d", len(svcList))
 	if err != nil {
 		log.Errorf("failed to get workloads in the env, error: %s", err)
 		return nil, err
