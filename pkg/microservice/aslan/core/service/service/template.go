940b7a9ed4a1fc2fd2a9f9f0dc9ec1cda037e36a
allenshen <shendongdong@koderover.com>
2022-12-28 11:07:23 +0800

    fix auto sync service var issue
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/service/service/template.go
+++ pkg/microservice/aslan/core/service/service/template.go
@@ -457,6 +457,7 @@ func reloadServiceFromYamlTemplate(userName, projectName string, template *commo
 	if service.AutoSync {
 		serviceVars = template.ServiceVars
 	}
+	log.Infof("######### service vars %v", serviceVars)
 
 	svc := &commonmodels.Service{
 		ServiceName:  service.ServiceName,
