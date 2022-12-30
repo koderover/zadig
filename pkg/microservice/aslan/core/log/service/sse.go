d695c7313750365797e28bd8784db2bf75a8177c
Min Min <jamsman94@gmail.com>
2022-12-30 10:53:00 +0800

    temporary PR
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/log/service/sse.go
+++ pkg/microservice/aslan/core/log/service/sse.go
@@ -183,13 +183,33 @@ func TaskContainerLogStream(ctx context.Context, streamChan chan interface{}, op
 		}
 		// Compatible with the situation where the old data has not been modified
 		if build != nil && build.PreBuild != nil && build.PreBuild.ClusterID != "" {
-			options.ClusterID = build.PreBuild.ClusterID
+			// since there are 2 cases in this situation: if no template is used, then we use the old logic
+			if build.TemplateID == "" {
+				options.ClusterID = build.PreBuild.ClusterID
 
-			switch build.PreBuild.ClusterID {
-			case setting.LocalClusterID:
-				options.Namespace = config.Namespace()
-			default:
-				options.Namespace = setting.AttachedClusterNamespace
+				switch build.PreBuild.ClusterID {
+				case setting.LocalClusterID:
+					options.Namespace = config.Namespace()
+				default:
+					options.Namespace = setting.AttachedClusterNamespace
+				}
+			} else {
+				log.Infof("starting to get logs for build with template")
+				// otherwise we have to get the template ID and find its cluster settings
+				template, err := commonrepo.NewBuildTemplateColl().Find(&commonrepo.BuildTemplateQueryOption{
+					ID: build.TemplateID,
+				})
+				if err != nil {
+					log.Errorf("failed to find build template of ID: [%s], error: [%s]", build.TemplateID, err)
+					return
+				}
+				options.ClusterID = template.PreBuild.ClusterID
+				switch template.PreBuild.ClusterID {
+				case setting.LocalClusterID:
+					options.Namespace = config.Namespace()
+				default:
+					options.Namespace = setting.AttachedClusterNamespace
+				}
 			}
 		}
 	}
