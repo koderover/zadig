95cbdcd17561e95011f67e9074797f4b184dcbc3
Min Min <minmin@koderover.com>
2023-01-10 16:28:22 +0800

    even more debug message
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/system/service/meego.go
+++ pkg/microservice/aslan/core/system/service/meego.go
@@ -114,6 +114,8 @@ func ListAvailableWorkItemTransitions(projectID, typeKey string, workItemID int)
 	}
 
 	log.Infof("getting work item info for work item ID: %d", workItemID)
+	log.Infof("getting work item info for work item type key: %s", typeKey)
+	log.Infof("getting work item info for project: %s", projectID)
 	workItem, err := client.GetWorkItem(projectID, typeKey, workItemID)
 	if err != nil {
 		return nil, err
