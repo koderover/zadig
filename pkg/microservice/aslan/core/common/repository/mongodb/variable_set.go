01b23728298a49b087d8f399ab9bf551a68df4ec
allenshen <shendongdong@koderover.com>
2023-01-19 14:31:21 +0800

    fix variable project name missing bug
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/common/repository/mongodb/variable_set.go
+++ pkg/microservice/aslan/core/common/repository/mongodb/variable_set.go
@@ -38,9 +38,10 @@ type VariableSetColl struct {
 }
 
 type VariableSetFindOption struct {
-	ID      string `json:"id"`
-	PerPage int    `json:"perPage"`
-	Page    int    `json:"page"`
+	ID          string `json:"id"`
+	PerPage     int    `json:"perPage"`
+	Page        int    `json:"page"`
+	ProjectName string `json:"projectName"`
 }
 
 func NewVariableSetColl() *VariableSetColl {
@@ -56,8 +57,11 @@ func (c *VariableSetColl) GetCollectionName() string {
 
 func (c *VariableSetColl) EnsureIndex(ctx context.Context) error {
 	mod := mongo.IndexModel{
-		Keys:    bson.M{"repo_name": 1},
-		Options: options.Index().SetUnique(false),
+		Keys: bson.D{
+			bson.E{Key: "name", Value: 1},
+			bson.E{Key: "project_name", Value: 1},
+		},
+		Options: options.Index().SetUnique(true),
 	}
 	_, err := c.Indexes().CreateOne(ctx, mod)
 	return err
@@ -126,6 +130,7 @@ func (c *VariableSetColl) Delete(id string) error {
 func (c *VariableSetColl) List(option *VariableSetFindOption) (int64, []*models.VariableSet, error) {
 	resp := make([]*models.VariableSet, 0)
 	query := bson.M{}
+	query["$or"] = []bson.M{{"project_name": bson.M{"$eq": option.ProjectName}}, {"production": bson.M{"$exists": false}}}
 
 	ctx := context.Background()
 
--- pkg/microservice/aslan/core/project/handler/variable.go
+++ pkg/microservice/aslan/core/project/handler/variable.go
@@ -56,6 +56,7 @@ func CreateVariableSet(c *gin.Context) {
 		return
 	}
 	args.UserName = ctx.UserName
+	args.ProjectName = c.Query("projectName")
 
 	ctx.Err = service.CreateVariableSet(args)
 }
