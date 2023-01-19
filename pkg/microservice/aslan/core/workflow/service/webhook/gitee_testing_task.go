2cc044bfa1eb825c89b702f81c679aa18cc145c6
M-Cosmosss <yuzhou@koderover.com>
2023-01-19 20:05:07 +0800

    clear debug
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/webhook/gitee_testing_task.go
+++ pkg/microservice/aslan/core/workflow/service/webhook/gitee_testing_task.go
@@ -22,7 +22,7 @@ import (
 
 	"github.com/hashicorp/go-multierror"
 	"go.uber.org/zap"
-	
+
 	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	commonmodels "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
 	commonrepo "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
