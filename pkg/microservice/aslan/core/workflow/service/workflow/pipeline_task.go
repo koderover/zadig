9871082ad8cd01ffcc17d49ecd23107b24cf69e6
M-Cosmosss <yuzhou@koderover.com>
2023-03-06 10:29:40 +0800

    wip
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/pipeline_task.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/pipeline_task.go
@@ -377,7 +377,7 @@ func ListPipelineTasksV2Result(name string, typeString config.PipelineType, quer
 			return new
 		}
 		t.WorkflowArgs = nil
-		for _, stage := range t.Stages {
+		for i, stage := range t.Stages {
 			stage.Desc = ""
 			stage.TypeName = string(stage.TaskType)
 			for _, subTask := range stage.SubTasks {
@@ -423,6 +423,9 @@ func ListPipelineTasksV2Result(name string, typeString config.PipelineType, quer
 					stage.EndTime = math.Max(stage.EndTime, t.EndTime)
 					stage.Error = getErrorMsg(stage.Error, t.Error)
 				case config.TaskResetImage, config.TaskDeploy:
+					if i > 0 && t.Stages[i-1].TaskType == config.TaskArtifact {
+						stage.TaskType = config.TaskArtifactDeploy
+					}
 					t, err := base.ToDeployTask(subTask)
 					if err != nil {
 						return nil, err
