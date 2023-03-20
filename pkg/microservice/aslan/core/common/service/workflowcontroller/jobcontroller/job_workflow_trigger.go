5e944106b880de8ce9bc1dec3baa1770d5ac6a98
M-Cosmosss <yuzhou@koderover.com>
2023-03-20 16:36:04 +0800

    wip cancel
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_workflow_trigger.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_workflow_trigger.go
@@ -73,15 +73,17 @@ func (c *WorkflowTriggerJobCtl) Run(ctx context.Context) {
 	}
 	var runningTasks = make(map[runningTask]*commonmodels.WorkflowTriggerEvent)
 	cancelAllRunningTasks := func() {
-		for task := range runningTasks {
+		for task, event := range runningTasks {
 			err := client.CancelWorkflowTaskV4(setting.WorkflowTriggerTaskCreator,
 				task.WorkflowName, task.TaskID)
 			if err != nil {
 				log.Errorf("WorkflowTriggerJobCtl: CancelWorkflowTaskV4 %s-%d err: %v", task.WorkflowName, task.TaskID, err)
 			} else {
 				log.Debugf("WorkflowTriggerJobCtl: CancelWorkflowTaskV4 %s-%d success", task.WorkflowName, task.TaskID)
+				event.Status = config.StatusCancelled
 			}
 		}
+		c.ack()
 	}
 	defer cancelAllRunningTasks()
 
