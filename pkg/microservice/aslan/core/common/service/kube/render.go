06c1ec1ccd18e355838c78fed8a5b4291e6a0345
allenshen <shendongdong@koderover.com>
2023-03-22 20:03:02 +0800

    fix service deploy preview
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/common/service/kube/render.go
+++ pkg/microservice/aslan/core/common/service/kube/render.go
@@ -440,7 +440,7 @@ func FetchCurrentAppliedYaml(option *GeneSvcYamlOption) (string, int, error) {
 	}
 
 	// service not deployed by zadig, should only be updated with images
-	if !option.UnInstall && !option.UpdateServiceRevision && curProductSvc != nil && !commonutil.ServiceDeployed(option.ServiceName, productInfo.ServiceDeployStrategy) {
+	if !option.UnInstall && !commonutil.ServiceDeployed(option.ServiceName, productInfo.ServiceDeployStrategy) {
 		manifest, _, err := fetchImportedManifests(option, productInfo, prodSvcTemplate)
 		return manifest, int(curProductSvc.Revision), err
 	}
