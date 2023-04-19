20c82fc75be014b483e4cf9f21468f9fd333062a
Patrick Zhao <zhaoyu@koderover.com>
2023-04-19 16:31:37 +0800

    add error log if is nil
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/service.go
+++ pkg/microservice/aslan/core/common/service/service.go
@@ -1227,6 +1227,7 @@ func buildServiceInfoInEnv(productInfo *commonmodels.Product, templateSvcs []*co
 				tmplSvc = productTemplateSvcMap[svcName]
 			}
 			if tmplSvc == nil {
+				log.Errorf("failed to find service %s in template service, updateRevision: %v", svcName, updateSvcRevision)
 				tmplSvc = &commonmodels.Service{
 					ServiceName: svcName,
 				}
@@ -1295,7 +1296,10 @@ func buildServiceInfoInEnv(productInfo *commonmodels.Product, templateSvcs []*co
 			if tmplSvc != nil {
 				latestValues = tmplSvc.HelmChart.ValuesYaml
 			} else {
-				return "", nil, errors.Errorf("failed to find service %s in template service", svcName)
+				log.Errorf("failed to find service %s in template service, updateRevision: %v", svcName, updateSvcRevision)
+				tmplSvc = &commonmodels.Service{
+					ServiceName: svcName,
+				}
 			}
 
 			mergedValues := ""
