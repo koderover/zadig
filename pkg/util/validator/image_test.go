1024bd62923ed3fc7d7511941d8d1fd328f279a5
M-Cosmosss <yuzhou@koderover.com>
2023-04-27 10:14:44 +0800

    wip test
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/util/validator/image_test.go
+++ pkg/util/validator/image_test.go
@@ -19,19 +19,57 @@ package validator
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
+var _ = Describe("IsValidImageName", func() {
+	It("returns false for an empty string", func() {
+		Expect(IsValidImageName("")).To(BeFalse())
+	})
+
+	It("returns false for an invalid character", func() {
+		Expect(IsValidImageName("ubuntu：123")).To(BeFalse())
+	})
+
+	It("returns true for a valid image name without tag", func() {
+		Expect(IsValidImageName("ubuntu")).To(BeTrue())
+	})
+
+	It("returns false for an invalid image name with invalid characters", func() {
+		Expect(IsValidImageName("{ubuntu")).To(BeFalse())
+	})
+
+	It("returns false for an invalid image name with tag containing colon only", func() {
+		Expect(IsValidImageName("ubuntu:")).To(BeFalse())
+	})
+
+	It("returns true for a valid image name with tag and registry", func() {
+		Expect(IsValidImageName("docker.io/ubuntu:123")).To(BeTrue())
+	})
+
+	It("returns true for a valid image name with tag 'latest'", func() {
+		Expect(IsValidImageName("ubuntu:latest")).To(BeTrue())
+	})
+
+	It("returns false for an invalid image name with tag containing hyphen", func() {
+		Expect(IsValidImageName("ubuntu{:latest")).To(BeFalse())
+	})
+
+	It("returns true for a valid image name with tag containing hyphen", func() {
+		Expect(IsValidImageName("ubuntu:la-test")).To(BeTrue())
+	})
+
+	It("returns false for an invalid image name with trailing slash in tag", func() {
+		Expect(IsValidImageName("ubuntu:la-test/")).To(BeFalse())
+	})
+
+	It("returns true for a valid image name with tag and registry containing dots and hyphens", func() {
+		Expect(IsValidImageName("kr.example.com/test/1/nginx:20060102150405")).To(BeTrue())
+	})
+})
+
 func TestIsImageName(t *testing.T) {
-	assert.False(t, IsValidImageName(""))
-	assert.False(t, IsValidImageName("ubuntu：123"))
-	assert.True(t, IsValidImageName("ubuntu"))
-	assert.False(t, IsValidImageName("{ubuntu"))
-	assert.False(t, IsValidImageName("ubuntu:"))
-	assert.True(t, IsValidImageName("docker.io/ubuntu:123"))
-	assert.True(t, IsValidImageName("ubuntu:latest"))
-	assert.False(t, IsValidImageName("ubuntu{:latest"))
-	assert.True(t, IsValidImageName("ubuntu:la-test"))
-	assert.False(t, IsValidImageName("ubuntu:la-test/"))
-	assert.True(t, IsValidImageName("kr.example.com/test/1/nginx:20060102150405"))
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Validator Suite")
 }
