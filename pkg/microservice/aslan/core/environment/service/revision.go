221d6c7dd7224b1dd912e7d467e0d4a199d7b58d
allenshen <shendongdong@koderover.com>
2023-05-08 19:47:59 +0800

    optimize service revision compare logic
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/environment/service/revision.go
+++ pkg/microservice/aslan/core/environment/service/revision.go
@@ -25,8 +25,6 @@ import (
 	commonmodels "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/models"
 	commonrepo "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
 	templaterepo "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb/template"
-	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/kube"
-	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/render"
 	commonutil "github.com/koderover/zadig/pkg/microservice/aslan/core/common/util"
 	"github.com/koderover/zadig/pkg/setting"
 	e "github.com/koderover/zadig/pkg/tool/errors"
@@ -144,22 +142,22 @@ func GetProductRevision(product *commonmodels.Product, allServiceTmpls []*common
 		prodRev.Updatable = true
 	}
 
-	var newRender, oldRender *commonmodels.RenderSet
-	if prodTmpl.ProductFeature == nil || prodTmpl.ProductFeature.DeployType == setting.K8SDeployType {
-		// TODO is it the right way to fetch new product?
-		newRender, err = render.GetRenderSet(product.Render.Name, 0, false, product.EnvName, log)
-		if err != nil {
-			return prodRev, err
-		}
-
-		oldRender, err = render.GetRenderSet(product.Render.Name, product.Render.Revision, false, product.EnvName, log)
-		if err != nil {
-			return prodRev, err
-		}
-	}
+	//var newRender, oldRender *commonmodels.RenderSet
+	//if prodTmpl.ProductFeature == nil || prodTmpl.ProductFeature.DeployType == setting.K8SDeployType {
+	//	// TODO is it the right way to fetch new product?
+	//	newRender, err = render.GetRenderSet(product.Render.Name, 0, false, product.EnvName, log)
+	//	if err != nil {
+	//		return prodRev, err
+	//	}
+	//
+	//	oldRender, err = render.GetRenderSet(product.Render.Name, product.Render.Revision, false, product.EnvName, log)
+	//	if err != nil {
+	//		return prodRev, err
+	//	}
+	//}
 
 	// 交叉对比已创建的服务组和服务组模板
-	prodRev.ServiceRevisions, err = compareGroupServicesRev(prodTmpl.Services, product, allServiceTmpls, newRender, oldRender, log)
+	prodRev.ServiceRevisions, err = compareGroupServicesRev(prodTmpl.Services, product, allServiceTmpls, log)
 	if err != nil {
 		log.Error(err)
 		return nil, e.ErrGetProductRevision.AddDesc(err.Error())
@@ -192,8 +190,7 @@ func GetProductRevision(product *commonmodels.Product, allServiceTmpls []*common
 // - product: the product environment instance
 // - maxServices: distinted service and max revision
 // - maxConfigs: distincted service config and max revision
-func compareGroupServicesRev(servicesTmpl [][]string, productInfo *commonmodels.Product, allServiceTmpls []*commonmodels.Service,
-	newRender *commonmodels.RenderSet, oldRender *commonmodels.RenderSet, log *zap.SugaredLogger) ([]*SvcRevision, error) {
+func compareGroupServicesRev(servicesTmpl [][]string, productInfo *commonmodels.Product, allServiceTmpls []*commonmodels.Service, log *zap.SugaredLogger) ([]*SvcRevision, error) {
 
 	var serviceRev []*SvcRevision
 	svcList := make([]*commonmodels.ProductService, 0)
@@ -232,7 +229,7 @@ func compareGroupServicesRev(servicesTmpl [][]string, productInfo *commonmodels.
 	}
 
 	var err error
-	serviceRev, err = compareServicesRev(svcTmplNameList, svcList, allServiceTmpls, newRender, oldRender, log)
+	serviceRev, err = compareServicesRev(svcTmplNameList, svcList, allServiceTmpls, log)
 	if err != nil {
 		log.Errorf("Failed to compare service revision, %s:%s, Error: %v", productInfo.ProductName, productInfo.EnvName, err)
 		return serviceRev, e.ErrListProductsRevision.AddDesc(err.Error())
@@ -246,13 +243,12 @@ func compareGroupServicesRev(servicesTmpl [][]string, productInfo *commonmodels.
 // - services: service list of product environment instance
 // - maxServices: distinted service and max revision
 // - maxConfigs: distincted service config and max revision
-func compareServicesRev(serviceTmplNames []string, services []*commonmodels.ProductService, allServiceTmpls []*commonmodels.Service,
-	newRender *commonmodels.RenderSet, oldRender *commonmodels.RenderSet, log *zap.SugaredLogger) ([]*SvcRevision, error) {
+func compareServicesRev(serviceTmplNames []string, productServices []*commonmodels.ProductService, allServiceTmpls []*commonmodels.Service, log *zap.SugaredLogger) ([]*SvcRevision, error) {
 
 	serviceRevs := make([]*SvcRevision, 0)
 
 	productServiceMap := make(map[string]*commonmodels.ProductService)
-	for _, service := range services {
+	for _, service := range productServices {
 		productServiceMap[service.ServiceName] = service
 	}
 
@@ -261,41 +257,44 @@ func compareServicesRev(serviceTmplNames []string, services []*commonmodels.Prod
 		serviceTmplMap[tmplName] = tmplName
 	}
 
-	for _, serviceTmplName := range serviceTmplNames {
-		// 如果已创建的服务不包括新增的服务模板, 则认为是新增服务
-		// 新增服务如果涉及多个部署方式，目前把所有部署方式都默认加进来
-		if _, ok := productServiceMap[serviceTmplName]; !ok {
-			newServiceTmpls, err := getMaxServices(allServiceTmpls, serviceTmplName)
-			if err != nil {
-				log.Error(err)
-				return serviceRevs, err
-			}
-			for _, serviceTmpl := range newServiceTmpls {
-				serviceRev := &SvcRevision{
-					ServiceName:  serviceTmpl.ServiceName,
-					Type:         serviceTmpl.Type,
-					NextRevision: serviceTmpl.Revision,
-					Updatable:    true,
-					New:          true,
-				}
-				if serviceTmpl.Type == setting.K8SDeployType {
-					serviceRev.Containers = make([]*commonmodels.Container, 0)
-					for _, container := range serviceTmpl.Containers {
-						serviceRev.Containers = append(serviceRev.Containers, &commonmodels.Container{
-							Image:     container.Image,
-							Name:      container.Name,
-							ImageName: util.GetImageNameFromContainerInfo(container.ImageName, container.Name),
-						})
-					}
-				}
-				serviceRevs = append(serviceRevs, serviceRev)
-			}
+	latestSvcTmplMap := make(map[string]*commonmodels.Service)
+	for _, serviceTmpl := range allServiceTmpls {
+		latestSvcTmplMap[serviceTmpl.ServiceName] = serviceTmpl
+	}
 
+	// services exist in template_service but not in product_service
+	// these services can be added into product
+	for _, serviceTmplName := range serviceTmplNames {
+		if _, ok := productServiceMap[serviceTmplName]; !ok {
+			latestSvcTmpl, ok := latestSvcTmplMap[serviceTmplName]
+			if !ok {
+				log.Errorf("service %s not found in allServiceTmpls", serviceTmplName)
+				return serviceRevs, fmt.Errorf("failed to find service: %s in allServiceTmpls", serviceTmplName)
+			}
+			serviceRev := &SvcRevision{
+				ServiceName:  latestSvcTmpl.ServiceName,
+				Type:         latestSvcTmpl.Type,
+				NextRevision: latestSvcTmpl.Revision,
+				Updatable:    true,
+				New:          true,
+			}
+			if latestSvcTmpl.Type == setting.K8SDeployType {
+				serviceRev.Containers = make([]*commonmodels.Container, 0)
+				for _, container := range latestSvcTmpl.Containers {
+					serviceRev.Containers = append(serviceRev.Containers, &commonmodels.Container{
+						Image:     container.Image,
+						Name:      container.Name,
+						ImageName: util.GetImageNameFromContainerInfo(container.ImageName, container.Name),
+					})
+				}
+			}
+			serviceRevs = append(serviceRevs, serviceRev)
 		}
 	}
 
-	for _, service := range services {
-		// 如果没有找到服务模板信息, 则认为服务给删除了
+	for _, service := range productServices {
+		// services exist in product but not in template services
+		// these services can be deleted from product
 		if _, ok := serviceTmplMap[service.ServiceName]; !ok {
 			serviceRev := &SvcRevision{
 				ServiceName: service.ServiceName,
@@ -307,24 +306,29 @@ func compareServicesRev(serviceTmplNames []string, services []*commonmodels.Prod
 			}
 			serviceRevs = append(serviceRevs, serviceRev)
 		} else {
-			maxServiceTmpl, err := getMaxServiceRevision(allServiceTmpls, service.ServiceName, service.ProductName)
-			if err != nil {
-				log.Errorf("Failed to get max service revision. Service: %s; Prodcut: %s; Error: %v",
-					service.ServiceName, service.ProductName, err)
-				return serviceRevs, err
+			//maxServiceTmpl, err := getMaxServiceRevision(allServiceTmpls, service.ServiceName, service.ProductName)
+			//if err != nil {
+			//	log.Errorf("Failed to get max service revision. Service: %s; Prodcut: %s; Error: %v",
+			//		service.ServiceName, service.ProductName, err)
+			//	return serviceRevs, err
+			//}
+			maxServiceTmpl, ok := latestSvcTmplMap[service.ServiceName]
+			if !ok {
+				log.Errorf("service %s not found in allServiceTmpls", service.ServiceName)
+				return serviceRevs, fmt.Errorf("failed to find service: %s in allServiceTmpls", service.ServiceName)
 			}
 
-			currentServiceTmpl, err := commonrepo.NewServiceColl().Find(&commonrepo.ServiceFindOption{
-				ServiceName: service.ServiceName,
-				ProductName: service.ProductName,
-				Revision:    service.Revision,
-				Type:        service.Type,
-			})
-			if err != nil {
-				log.Errorf("Failed to get service by revision. Service: %s; Product: %s; Type: %s; Revision: %d; Error: %v",
-					service.ServiceName, service.ProductName, service.Type, service.Revision, err)
-				return serviceRevs, err
-			}
+			//currentServiceTmpl, err := commonrepo.NewServiceColl().Find(&commonrepo.ServiceFindOption{
+			//	ServiceName: service.ServiceName,
+			//	ProductName: service.ProductName,
+			//	Revision:    service.Revision,
+			//	Type:        service.Type,
+			//})
+			//if err != nil {
+			//	log.Errorf("Failed to get service by revision. Service: %s; Product: %s; Type: %s; Revision: %d; Error: %v",
+			//		service.ServiceName, service.ProductName, service.Type, service.Revision, err)
+			//	return serviceRevs, err
+			//}
 
 			serviceRev := &SvcRevision{
 				ServiceName:     service.ServiceName,
@@ -360,11 +364,11 @@ func compareServicesRev(serviceTmplNames []string, services []*commonmodels.Prod
 					serviceRev.Containers = append(serviceRev.Containers, c)
 				}
 
-				// 交叉对比已创建的配置和待更新配置模板
-				// 检查模板yaml渲染后是否有变化
-				if isRenderedStringUpdatable(currentServiceTmpl, maxServiceTmpl, oldRender, newRender) {
-					serviceRev.Updatable = true
-				}
+				//// 交叉对比已创建的配置和待更新配置模板
+				//// 检查模板yaml渲染后是否有变化
+				//if isRenderedStringUpdatable(currentServiceTmpl, maxServiceTmpl, oldRender, newRender) {
+				//	serviceRev.Updatable = true
+				//}
 
 				serviceRevs = append(serviceRevs, serviceRev)
 			} else if service.Type == setting.HelmDeployType {
@@ -377,72 +381,37 @@ func compareServicesRev(serviceTmplNames []string, services []*commonmodels.Prod
 	return serviceRevs, nil
 }
 
-// 分别获取三种type的最大revision对应的service
-func getMaxServices(services []*commonmodels.Service, serviceName string) ([]*commonmodels.Service, error) {
-	var (
-		resp        []*commonmodels.Service
-		k8sService  = &commonmodels.Service{}
-		helmService = &commonmodels.Service{}
-		pmService   = &commonmodels.Service{}
-	)
+//func getMaxServiceRevision(services []*commonmodels.Service, serviceName, productName string) (*commonmodels.Service, error) {
+//	resp := &commonmodels.Service{}
+//	for _, service := range services {
+//		if service.ServiceName == serviceName && service.ProductName == productName && service.Revision > resp.Revision {
+//			resp = service
+//		}
+//	}
+//	if resp.ServiceName == "" {
+//		return resp, fmt.Errorf("[%s] no service found", serviceName)
+//	}
+//	return resp, nil
+//}
 
-	for _, service := range services {
-		if service.ServiceName == serviceName && service.Type == setting.K8SDeployType && service.Revision > k8sService.Revision {
-			k8sService = service
-		}
-		if service.ServiceName == serviceName && service.Type == setting.PMDeployType && service.Revision > pmService.Revision {
-			pmService = service
-		}
-		if service.ServiceName == serviceName && service.Type == setting.HelmDeployType && service.Revision > helmService.Revision {
-			helmService = service
-		}
-	}
-	if k8sService.ServiceName != "" {
-		resp = append(resp, k8sService)
-	}
-	if pmService.ServiceName != "" {
-		resp = append(resp, pmService)
-	}
-	if helmService.ServiceName != "" {
-		resp = append(resp, helmService)
-	}
-	if len(resp) <= 0 {
-		return resp, fmt.Errorf("[%s] no service found", serviceName)
-	}
-	return resp, nil
-}
-
-func getMaxServiceRevision(services []*commonmodels.Service, serviceName, productName string) (*commonmodels.Service, error) {
-	resp := &commonmodels.Service{}
-	for _, service := range services {
-		if service.ServiceName == serviceName && service.ProductName == productName && service.Revision > resp.Revision {
-			resp = service
-		}
-	}
-	if resp.ServiceName == "" {
-		return resp, fmt.Errorf("[%s] no service found", serviceName)
-	}
-	return resp, nil
-}
-
-func isRenderedStringUpdatable(currentSvc, nextSvc *commonmodels.Service, currentRender, nextRender *commonmodels.RenderSet) bool {
-	resp := false
-	currentString, nextString := currentSvc.Yaml, nextSvc.Yaml
-	currentSvcVars, nextSvcVars := currentSvc.ServiceVars, nextSvc.ServiceVars
-
-	currentString, err := kube.RenderServiceYaml(currentString, "", "", currentRender, currentSvcVars, currentSvc.VariableYaml)
-	if err != nil {
-		//log.Errorf("failed to check is RenderedString updatable, err: %s", err)
-		return false
-	}
-	nextString, err = kube.RenderServiceYaml(nextString, "", "", nextRender, nextSvcVars, nextSvc.VariableYaml)
-	if err != nil {
-		//log.Errorf("failed to check is RenderedString updatable, err: %s", err)
-		return false
-	}
-	if currentString != nextString {
-		resp = true
-	}
-	return resp
-
-}
+//func isRenderedStringUpdatable(currentSvc, nextSvc *commonmodels.Service, currentRender, nextRender *commonmodels.RenderSet) bool {
+//	resp := false
+//	currentString, nextString := currentSvc.Yaml, nextSvc.Yaml
+//	currentSvcVars, nextSvcVars := currentSvc.ServiceVars, nextSvc.ServiceVars
+//
+//	currentString, err := kube.RenderServiceYaml(currentString, "", "", currentRender, currentSvcVars, currentSvc.VariableYaml)
+//	if err != nil {
+//		//log.Errorf("failed to check is RenderedString updatable, err: %s", err)
+//		return false
+//	}
+//	nextString, err = kube.RenderServiceYaml(nextString, "", "", nextRender, nextSvcVars, nextSvc.VariableYaml)
+//	if err != nil {
+//		//log.Errorf("failed to check is RenderedString updatable, err: %s", err)
+//		return false
+//	}
+//	if currentString != nextString {
+//		resp = true
+//	}
+//	return resp
+//
+//}
