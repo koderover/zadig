99e327578ba0dbec90b17fe978f27062627c3a07
M-Cosmosss <yuzhou@koderover.com>
2023-05-23 14:25:18 +0800

    fix comment
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/system/handler/project_management.go
+++ pkg/microservice/aslan/core/system/handler/project_management.go
@@ -98,7 +98,7 @@ func SearchJiraProjectIssuesWithJQL(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
 
-	// 5.22 summary only support {{.System.USERNAME}} variable
+	// 5.22 JQL only support {{.System.USERNAME}} variable
 	// refactor if more variables are needed
 	ctx.Resp, ctx.Err = service.SearchJiraProjectIssuesWithJQL(c.Query("project"), strings.ReplaceAll(c.Query("jql"), "{{.System.USERNAME}}", ctx.UserName), c.Query("summary"))
 }
