1b888eefb0ebd3281c3a392beebffb4f0163ecf4
shendongdong <shendongdong@koderover.com>
2023-05-31 10:04:58 +0800

    vars ua change (#2666)
    * add basic logic
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * add ua logic
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * add ua logic for service vars
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * optimize data
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * optimize code
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * fix conversions
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * remvove warning
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    ---------
    
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/cli/upgradeassistant/cmd/migrate/1180.go
+++ pkg/cli/upgradeassistant/cmd/migrate/1180.go
@@ -42,9 +42,15 @@ func init() {
 func V1170ToV1180() error {
 	if err := migrateJiraAuthType(); err != nil {
 		log.Errorf("migrateJiraAuthType err: %v", err)
+		return errors.Wrapf(err, "failed to execute migrateJiraAuthType")
 	}
 	if err := migrateSystemTheme(); err != nil {
 		log.Errorf("migrateSystemTheme err: %v", err)
+		return errors.Wrapf(err, "failed to execute migrateSystemTheme")
+	}
+	if err := migrateVariables(); err != nil {
+		log.Errorf("migrateVariables err: %v", err)
+		return errors.Wrapf(err, "failed to execute migrateVariables")
 	}
 	if err := migrateWorkflowV4LarkApproval(); err != nil {
 		log.Errorf("migrateWorkflowV4LarkApproval err: %v", err)
