a5bf43f7ce859a873d799c5ae006d070ea9de77e
Patrick Zhao <zhaoyu@koderover.com>
2023-06-08 16:03:44 +0800

    add proxy for llm client
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/llm.go
+++ pkg/microservice/aslan/core/common/service/llm.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/koderover/zadig/pkg/microservice/aslan/config"
 	commonrepo "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
 	"github.com/koderover/zadig/pkg/tool/llm"
 )
@@ -14,17 +15,21 @@ func GetLLMClient(ctx context.Context, name string) (llm.ILLM, error) {
 		return nil, fmt.Errorf("Could find the llm integration for %s: %w", name, err)
 	}
 
-	config := llm.LLMConfig{
+	llmConfig := llm.LLMConfig{
 		Name:    llmIntegration.Name,
 		Token:   llmIntegration.Token,
 		BaseURL: llmIntegration.BaseURL,
 	}
+	if llmIntegration.EnableProxy {
+		llmConfig.Proxy = config.ProxyHTTPSAddr()
+	}
+
 	llmClient, err := llm.NewClient(name)
 	if err != nil {
 		return nil, fmt.Errorf("Could not create the llm client for %s: %w", name, err)
 	}
 
-	err = llmClient.Configure(config)
+	err = llmClient.Configure(llmConfig)
 	if err != nil {
 		return nil, fmt.Errorf("Could not configure the llm client for %s: %w", name, err)
 	}
