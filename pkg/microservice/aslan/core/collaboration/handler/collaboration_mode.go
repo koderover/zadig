1c1a0f3f2e7838ac7a9a225e6fd986e1a070352b
Petrus <zhaoyu@koderover.com>
2023-07-10 19:56:19 +0800

    add detail log for collaboration mode (#2813) (#2830)
    * add detail log for collaboration mode
    
    
    
    * add delta log message feature
    
    
    
    ---------
    
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/collaboration/handler/collaboration_mode.go
+++ pkg/microservice/aslan/core/collaboration/handler/collaboration_mode.go
@@ -18,15 +18,21 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	commonmodels "github.com/koderover/zadig/pkg/microservice/aslan/core/collaboration/repository/models"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/collaboration/service"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/collaboration"
+	"github.com/koderover/zadig/pkg/microservice/user/core/service/user"
 	internalhandler "github.com/koderover/zadig/pkg/shared/handler"
 	e "github.com/koderover/zadig/pkg/tool/errors"
 	"github.com/koderover/zadig/pkg/tool/log"
+	"github.com/koderover/zadig/pkg/types"
 )
 
 func GetCollaborationMode(c *gin.Context) {
@@ -57,7 +63,13 @@ func CreateCollaborationMode(c *gin.Context) {
 		return
 	}
 
-	internalhandler.InsertOperationLog(c, ctx.Account, args.ProjectName, "新增", "协作模式", args.Name, string(data), ctx.Logger)
+	detail, err := generateCollaborationDetailLog(ctx.Account, args, ctx.Logger)
+	if err != nil {
+		ctx.Err = e.ErrInvalidParam.AddDesc(err.Error())
+		return
+	}
+
+	internalhandler.InsertOperationLog(c, ctx.Account, args.ProjectName, "新增", "协作模式", detail, string(data), ctx.Logger)
 
 	ctx.Err = service.CreateCollaborationMode(ctx.Account, args, ctx.Logger)
 }
@@ -79,7 +91,13 @@ func UpdateCollaborationMode(c *gin.Context) {
 		return
 	}
 
-	internalhandler.InsertOperationLog(c, ctx.Account, args.ProjectName, "更新", "协作模式", args.Name, string(data), ctx.Logger)
+	detail, err := generateCollaborationDetailLog(ctx.Account, args, ctx.Logger)
+	if err != nil {
+		ctx.Err = e.ErrInvalidParam.AddDesc(err.Error())
+		return
+	}
+
+	internalhandler.InsertOperationLog(c, ctx.Account, args.ProjectName, "更新", "协作模式", detail, string(data), ctx.Logger)
 
 	ctx.Err = service.UpdateCollaborationMode(ctx.Account, args, ctx.Logger)
 }
@@ -97,4 +115,255 @@ func DeleteCollaborationMode(c *gin.Context) {
 	internalhandler.InsertOperationLog(c, ctx.Account, projectName, "删除", "协作模式", name, "", ctx.Logger)
 
 	ctx.Err = service.DeleteCollaborationMode(ctx.Account, projectName, name, ctx.Logger)
+}
+
+func generateCollaborationDetailLog(username string, args *commonmodels.CollaborationMode, logger *zap.SugaredLogger) (string, error) {
+	collaborationWorkflowVerbTranslateMap := map[string]string{
+		"get_workflow":   "查看",
+		"edit_workflow":  "编辑",
+		"run_workflow":   "执行",
+		"debug_workflow": "调试",
+	}
+
+	collaborationProductVerbTranslateMap := map[string]string{
+		"get_environment":             "查看",
+		"config_environment":          "配置",
+		"manage_environment":          "管理服务实例",
+		"debug_pod":                   "服务调试",
+		"get_production_environment":  "查看",
+		"edit_production_environment": "编辑",
+		"production_debug_pod":        "服务调试",
+	}
+
+	collaborationTypeTranslateMap := map[string]string{
+		"new":   "独享",
+		"share": "共享",
+	}
+
+	currMode, exists, err := service.GetCollaborationMode(username, args.ProjectName, args.Name, logger)
+	if err != nil {
+		return "", fmt.Errorf("can't get current collaboration mode, projectName: %v, name: %v err: %v", args.ProjectName, args.Name, err)
+	}
+	if !exists {
+		currMode = &commonmodels.CollaborationMode{
+			Name:      args.Name,
+			Members:   []string{},
+			Workflows: []commonmodels.WorkflowCMItem{},
+			Products:  []commonmodels.ProductCMItem{},
+		}
+	}
+
+	currUserIDSet := sets.NewString(currMode.Members...)
+	argsUserIDSet := sets.NewString(args.Members...)
+	addedUserIDSet := argsUserIDSet.Difference(currUserIDSet)
+	deletedUserIDSet := currUserIDSet.Difference(argsUserIDSet)
+
+	allUserIDSet := sets.NewString(args.Members...)
+	allUserIDSet.Insert(currMode.Members...)
+
+	detail := "协作模式名称：" + args.Name + "\n\n"
+
+	// user part
+	userResp, err := user.SearchUsersByUIDs(allUserIDSet.List(), logger)
+	if err != nil {
+		return "", fmt.Errorf("failed to search users by uids(%v), err: %v", args.Members, err)
+	}
+	userInfoMap := make(map[string]types.UserInfo)
+	for _, user := range userResp.Users {
+		userInfoMap[user.Uid] = user
+	}
+
+	if deletedUserIDSet.Len() != 0 {
+		detail += "删除用户："
+		for _, userid := range deletedUserIDSet.List() {
+			detail += userInfoMap[userid].Name + "，"
+		}
+		detail = strings.Trim(detail, "，")
+		detail += "\n\n"
+	}
+
+	if addedUserIDSet.Len() != 0 {
+		detail += "新增用户："
+		for _, userid := range addedUserIDSet.List() {
+			detail += userInfoMap[userid].Name + "，"
+		}
+		detail = strings.Trim(detail, "，")
+		detail += "\n"
+
+		for _, workflow := range args.Workflows {
+			detail += "工作流名称：" + workflow.Name + "，类型：" + collaborationTypeTranslateMap[string(workflow.CollaborationType)] + "，"
+			detail += "权限："
+			for _, verb := range workflow.Verbs {
+				detail += collaborationWorkflowVerbTranslateMap[verb] + "，"
+			}
+			detail = strings.TrimSuffix(detail, "，")
+			detail += "\n"
+		}
+
+		for _, env := range args.Products {
+			detail += "环境名称：" + env.Name + "，类型：" + collaborationTypeTranslateMap[string(env.CollaborationType)] + "，权限："
+			for _, verb := range env.Verbs {
+				detail += collaborationProductVerbTranslateMap[verb] + "，"
+			}
+
+			detail = strings.TrimSuffix(detail, "，")
+			detail += "\n"
+		}
+
+		detail += "\n"
+	}
+
+	// workflow part
+	currWorklowMap := make(map[string]commonmodels.WorkflowCMItem)
+	argWorklowMap := make(map[string]commonmodels.WorkflowCMItem)
+	currWorklowSet := sets.NewString()
+	argWorklowSet := sets.NewString()
+
+	for _, workflow := range currMode.Workflows {
+		currWorklowSet.Insert(workflow.Name)
+		currWorklowMap[workflow.Name] = workflow
+	}
+	for _, workflow := range args.Workflows {
+		argWorklowSet.Insert(workflow.Name)
+		argWorklowMap[workflow.Name] = workflow
+	}
+
+	addedWorkflowSet := argWorklowSet.Difference(currWorklowSet)
+	deletedWorkflowSet := currWorklowSet.Difference(argWorklowSet)
+	intersectionWorkflowSet := argWorklowSet.Intersection(currWorklowSet)
+
+	detail += "权限变更：\n"
+	for _, name := range addedWorkflowSet.List() {
+		workflow := argWorklowMap[name]
+		detail += "工作流名称：" + workflow.Name + "，类型：" + collaborationTypeTranslateMap[string(workflow.CollaborationType)] + "，"
+		detail += "增加权限："
+		for _, verb := range workflow.Verbs {
+			detail += collaborationWorkflowVerbTranslateMap[verb] + "，"
+		}
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+	for _, name := range deletedWorkflowSet.List() {
+		workflow := currWorklowMap[name]
+		detail += "工作流名称：" + workflow.Name + "，类型：" + collaborationTypeTranslateMap[string(workflow.CollaborationType)] + "，"
+		detail += "移除权限："
+		for _, verb := range workflow.Verbs {
+			detail += collaborationWorkflowVerbTranslateMap[verb] + "，"
+		}
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+	for _, name := range intersectionWorkflowSet.List() {
+		workflow := argWorklowMap[name]
+		currWorkflow := currWorklowMap[name]
+
+		currVerbSet := sets.NewString(currWorkflow.Verbs...)
+		argVerbSet := sets.NewString(workflow.Verbs...)
+		addedVerbSet := argVerbSet.Difference(currVerbSet)
+		deletedVerbSet := currVerbSet.Difference(argVerbSet)
+
+		if addedVerbSet.Len() == 0 && deletedVerbSet.Len() == 0 {
+			continue
+		}
+
+		detail += "工作流名称：" + workflow.Name + "，类型：" + collaborationTypeTranslateMap[string(workflow.CollaborationType)] + "，"
+		if addedVerbSet.Len() != 0 {
+			detail += "增加权限："
+			for _, verb := range addedVerbSet.List() {
+				detail += collaborationWorkflowVerbTranslateMap[verb] + "，"
+			}
+		}
+		if deletedVerbSet.Len() != 0 {
+			detail += "移除权限："
+			for _, verb := range deletedVerbSet.List() {
+				detail += collaborationWorkflowVerbTranslateMap[verb] + "，"
+			}
+		}
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+
+	// product part
+	currProductMap := make(map[string]commonmodels.ProductCMItem)
+	argProductMap := make(map[string]commonmodels.ProductCMItem)
+	currProductSet := sets.NewString()
+	argProductSet := sets.NewString()
+
+	for _, product := range currMode.Products {
+		currProductSet.Insert(product.Name)
+		currProductMap[product.Name] = product
+	}
+	for _, product := range args.Products {
+		argProductSet.Insert(product.Name)
+		argProductMap[product.Name] = product
+	}
+
+	addedProductSet := argProductSet.Difference(currProductSet)
+	deletedProductSet := currProductSet.Difference(argProductSet)
+	intersectionProductSet := argProductSet.Intersection(currProductSet)
+
+	for _, envName := range addedProductSet.List() {
+		env := argProductMap[envName]
+		detail += "环境名称：" + env.Name + "，类型：" + collaborationTypeTranslateMap[string(env.CollaborationType)] + "，新增权限："
+		for _, verb := range env.Verbs {
+			detail += collaborationProductVerbTranslateMap[verb] + "，"
+		}
+
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+	for _, envName := range deletedProductSet.List() {
+		env := currProductMap[envName]
+		detail += "环境名称：" + env.Name + "，类型：" + collaborationTypeTranslateMap[string(env.CollaborationType)] + "，移除权限："
+		for _, verb := range env.Verbs {
+			detail += collaborationProductVerbTranslateMap[verb] + "，"
+		}
+
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+	for _, envName := range intersectionProductSet.List() {
+		argProduct := argProductMap[envName]
+		currProduct := currProductMap[envName]
+
+		currVerbSet := sets.NewString(currProduct.Verbs...)
+		argVerbSet := sets.NewString(argProduct.Verbs...)
+		addedVerbSet := argVerbSet.Difference(currVerbSet)
+		deletedVerbSet := currVerbSet.Difference(argVerbSet)
+
+		if addedVerbSet.Len() == 0 && deletedVerbSet.Len() == 0 {
+			continue
+		}
+
+		detail += "环境名称：" + argProduct.Name + "，类型：" + collaborationTypeTranslateMap[string(argProduct.CollaborationType)] + "，"
+
+		if addedVerbSet.Len() != 0 {
+			detail += "增加权限："
+			for _, verb := range addedVerbSet.List() {
+				detail += collaborationProductVerbTranslateMap[verb] + "，"
+			}
+		}
+		if deletedVerbSet.Len() != 0 {
+			detail += "移除权限："
+			for _, verb := range deletedVerbSet.List() {
+				detail += collaborationProductVerbTranslateMap[verb] + "，"
+			}
+		}
+
+		detail = strings.TrimSuffix(detail, "，")
+		detail += "\n"
+	}
+
+	if strings.LastIndex(detail, "权限变更：\n") == len(detail)-len("权限变更：\n") {
+		detail = strings.TrimSuffix(detail, "权限变更：\n")
+		return detail, nil
+	}
+
+	detail += "影响用户："
+	for _, userID := range allUserIDSet.List() {
+		detail += userInfoMap[userID].Name + "，"
+	}
+	detail = strings.TrimSuffix(detail, "，")
+
+	return detail, nil
 }
