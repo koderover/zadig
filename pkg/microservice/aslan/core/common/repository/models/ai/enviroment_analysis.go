68a3284ad5c5a27b1d5f088f4120f0e7f1e49d3a
zhatiai <86452931+zhatiai@users.noreply.github.com>
2023-07-19 15:10:20 +0800

    fix: remove ai env analysis db unique index (#2816)
    Signed-off-by: zhatiai <zhatiai@koderover.com>
    Co-authored-by: zhatiai <zhatiai@koderover.com>

--- pkg/microservice/aslan/core/common/repository/models/ai/enviroment_analysis.go
+++ pkg/microservice/aslan/core/common/repository/models/ai/enviroment_analysis.go
@@ -20,18 +20,18 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type EnvAIAnalysis struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"                json:"id,omitempty"`
-	ProjectName string             `bson:"project_name" json:"project_name"`
-	DeployType  string             `bson:"deploy_type" json:"deploy_type"`
-	EnvName     string             `bson:"env_name" json:"env_name"`
-	Production  bool               `bson:"production" json:"production"`
-	Status      string             `bson:"status" json:"status"`
-	Result      string             `bson:"result" json:"result"`
-	Err         string             `bson:"err" json:"err"`
-	StartTime   int64              `bson:"start_time" json:"start_time"`
-	EndTime     int64              `bson:"end_time" json:"end_time"`
-	Updated     int64              `bson:"updated" json:"updated"`
-	TriggerName string             `bson:"trigger_name" json:"trigger_name"`
-	CreatedBy   string             `bson:"created_by" json:"created_by"`
+	ProjectName string             `bson:"project_name"                 json:"project_name"`
+	DeployType  string             `bson:"deploy_type"                  json:"deploy_type"`
+	EnvName     string             `bson:"env_name"                     json:"env_name"`
+	Production  bool               `bson:"production"                   json:"production"`
+	Status      string             `bson:"status"                       json:"status"`
+	Result      string             `bson:"result"                       json:"result"`
+	Err         string             `bson:"err"                          json:"err"`
+	StartTime   int64              `bson:"start_time"                   json:"start_time"`
+	EndTime     int64              `bson:"end_time"                     json:"end_time"`
+	Updated     int64              `bson:"updated"                      json:"updated"`
+	TriggerName string             `bson:"trigger_name"                 json:"trigger_name"`
+	CreatedBy   string             `bson:"created_by"                   json:"created_by"`
 }
 
 func (EnvAIAnalysis) TableName() string {
