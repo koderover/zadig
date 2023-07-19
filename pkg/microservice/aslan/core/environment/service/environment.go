f36cf592695a0a1c4185b5debb55144aa48d8e42
Petrus <zhaoyu@koderover.com>
2023-07-19 15:03:31 +0800

    set default analysis config when create new env (#2807)
    * set default analysis config when create new env
    
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>
    
    * add migrateProductEnvAnalysisConfig func
    
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>
    
    ---------
    
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/environment/service/environment.go
+++ pkg/microservice/aslan/core/environment/service/environment.go
@@ -2369,6 +2369,24 @@ func preCreateProduct(envName string, args *commonmodels.Product, kubeClient cli
 		tmpRenderInfo.Revision = args.Render.Revision
 	}
 
+	if productTmpl.ProductFeature.DeployType == setting.HelmDeployType || productTmpl.ProductFeature.DeployType == setting.K8SDeployType {
+		args.AnalysisConfig = &commonmodels.AnalysisConfig{
+			ResourceTypes: []commonmodels.ResourceType{
+				commonmodels.ResourceTypePod,
+				commonmodels.ResourceTypeDeployment,
+				commonmodels.ResourceTypeReplicaSet,
+				commonmodels.ResourceTypePVC,
+				commonmodels.ResourceTypeService,
+				commonmodels.ResourceTypeIngress,
+				commonmodels.ResourceTypeStatefulSet,
+				commonmodels.ResourceTypeCronJob,
+				commonmodels.ResourceTypeHPA,
+				commonmodels.ResourceTypePDB,
+				commonmodels.ResourceTypeNetworkPolicy,
+			},
+		}
+	}
+
 	args.Render = tmpRenderInfo
 	if preCreateNSAndSecret(productTmpl.ProductFeature) {
 		return ensureKubeEnv(args.Namespace, args.RegistryID, map[string]string{setting.ProductLabel: args.ProductName}, args.ShareEnv.Enable, kubeClient, log)
@@ -3182,7 +3200,7 @@ func EnvAnalysis(projectName, envName string, production *bool, triggerName stri
 		config.HubServerAddress(), env.ClusterID,
 		llmClient,
 		filters, env.GetNamespace(),
-		true,  // noCache bool
+		false, // noCache bool
 		true,  // explain bool
 		10,    // maxConcurrency int
 		false, // withDoc bool
