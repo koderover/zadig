fd18ec5244134ef1c54bab168a3b9da798ba714a
zhatiai <86452931+zhatiai@users.noreply.github.com>
2023-07-27 14:50:20 +0800

    ai analyze build log (#2743)
    * feat: ai analysis build log
    
    Signed-off-by: zhatiai <zhatiai@koderover.com>
    
    * fix some review problems
    
    Signed-off-by: zhatiai <zhatiai@koderover.com>
    
    ---------
    
    Signed-off-by: zhatiai <zhatiai@koderover.com>
    Co-authored-by: zhatiai <zhatiai@koderover.com>

--- pkg/microservice/aslan/core/log/handler/router.go
+++ pkg/microservice/aslan/core/log/handler/router.go
@@ -36,6 +36,7 @@ func (*Router) Inject(router *gin.RouterGroup) {
 		log.GET("/v3/workflow/:workflowName/tasks/:taskId", GetWorkflowBuildV3JobContainerLogs)
 		log.GET("/scanning/:id/task/:scan_id", GetScanningContainerLogs)
 		log.GET("/v4/workflow/:workflowName/tasks/:taskID/jobs/:jobName", GetWorkflowV4JobContainerLogs)
+		log.POST("/ai/workflow/:workflowName/tasks/:taskID/jobs/:jobName", AIAnalyzeBuildLog)
 	}
 
 	sse := router.Group("sse")
