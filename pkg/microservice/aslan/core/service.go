70a7e1fb7308cb3edc9b3fabee93b5724a08833b
zhatiai <zhatiai@koderover.com>
2023-07-28 08:08:51 +0800

    fix init-admin-user problem
    Signed-off-by: zhatiai <zhatiai@koderover.com>

--- pkg/microservice/aslan/core/service.go
+++ pkg/microservice/aslan/core/service.go
@@ -24,6 +24,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	newgoCron "github.com/go-co-op/gocron"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/hashicorp/go-multierror"
@@ -40,6 +41,7 @@ import (
 	commonrepo "github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/repository/mongodb/template"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/kube"
+	commonuser "github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/user"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/webhook"
 	"github.com/koderover/zadig/pkg/microservice/aslan/core/common/service/workflowcontroller"
 	environmentservice "github.com/koderover/zadig/pkg/microservice/aslan/core/environment/service"
@@ -69,6 +71,7 @@ import (
 	mongotool "github.com/koderover/zadig/pkg/tool/mongo"
 	"github.com/koderover/zadig/pkg/tool/rsa"
 	"github.com/koderover/zadig/pkg/types"
+	"github.com/koderover/zadig/pkg/util/ginzap"
 )
 
 const (
@@ -180,6 +183,8 @@ func Start(ctx context.Context) {
 	policyservice.MigratePolicyData()
 
 	initCron()
+
+	initUser()
 }
 
 func Stop(ctx context.Context) {
@@ -541,5 +546,24 @@ func InitializeUserDBAndTables() {
 
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func initUser() {
+	//init default admin user
+	c := new(gin.Context)
+	logger := ginzap.WithContext(c).Sugar()
+	log.Infof("============================ start to init default admin user ============================")
+	uid, needBind, err := commonuser.PresetSystemAdmin(commonconfig.AdminEmail(), commonconfig.AdminPassword(), commonconfig.SystemAddress(), logger)
+	if err != nil {
+		log.Errorf("aslan preset system admin err:%s", err)
+		return
+	}
+	if needBind {
+		err = commonuser.PresetRoleBinding(c, uid, logger)
+		if err != nil {
+			log.Errorf("aslan preset role binding err:%s", err)
+			return
+		}
 	}
 }
