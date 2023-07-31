ecb77c47beee0510776b392616ad3012a81720ac
allenshen <shendongdong@koderover.com>
2023-07-31 17:08:58 +0800

    remove debug log
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/service/service/service.go
+++ pkg/microservice/aslan/core/service/service/service.go
@@ -1216,16 +1216,13 @@ func UpdateProductionServiceReleaseNamingRule(userName, requestID, projectName s
 		}
 	}
 
-	log.Infof("-------- product count: %v", len(products))
 	// check if the release name already exists
 	for _, product := range products {
 		releaseName := util.GeneReleaseName(args.NamingRule, product.ProductName, product.Namespace, product.EnvName, args.ServiceName)
-		log.Infof("releaseName: %s", releaseName)
 		releaseNameMap, err := commonutil.GetReleaseNameToChartNameMap(product)
 		if err != nil {
 			return fmt.Errorf("failed to get release name to chart name map, err: %s", err)
 		}
-		log.Infof("releaseNameMap: %v", releaseNameMap)
 		if chartOrSvcName, ok := releaseNameMap[releaseName]; ok && chartOrSvcName != args.ServiceName {
 			return fmt.Errorf("release name %s already exists for chart or service %s in environment: %s", releaseName, chartOrSvcName, product.EnvName)
 		}
