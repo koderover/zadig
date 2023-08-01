99363693be66f68f7ac562f9ca47e6c4dbaaddbc
shendongdong <shendongdong@koderover.com>
2023-08-01 16:30:40 +0800

    fix aslan panic when fetching service details (#2905)
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/environment/service/service.go
+++ pkg/microservice/aslan/core/environment/service/service.go
@@ -218,9 +218,9 @@ func GetService(envName, productName, serviceName string, production bool, workL
 			}
 			ret.Scales = append(ret.Scales, mseResp.Scales...)
 			ret.Services = append(ret.Services, mseResp.Services...)
-			ret.Workloads = nil
-			ret.Namespace = env.Namespace
 		}
+		ret.Workloads = nil
+		ret.Namespace = env.Namespace
 	} else {
 		ret, err = GetServiceImpl(serviceName, serviceTmpl, workLoadType, env, clientset, inf, log)
 		if err != nil {
