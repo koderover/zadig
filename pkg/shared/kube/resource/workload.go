d472e65454808139d052eaa7b4fbc7acc9d285c9
M-Cosmosss <yuzhou@koderover.com>
2023-08-02 17:41:01 +0800

    wip
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/shared/kube/resource/workload.go
+++ pkg/shared/kube/resource/workload.go
@@ -16,8 +16,6 @@ limitations under the License.
 
 package resource
 
-import "github.com/koderover/zadig/pkg/microservice/aslan/config"
-
 type Workload struct {
 	Name     string           `json:"name"`
 	Type     string           `json:"type"`
@@ -26,8 +24,8 @@ type Workload struct {
 	Replicas int32            `json:"replicas"`
 	// ZadigXReleaseType represent the release type of workload created by zadigx when it is not empty
 	// frontend should limit or allow some operations on these workloads
-	ZadigXReleaseType config.ZadigXReleaseType `json:"zadigx_release_type"`
-	ZadigXReleaseTag  string                   `json:"zadigx_release_tag"`
+	ZadigXReleaseType string `json:"zadigx_release_type"`
+	ZadigXReleaseTag  string `json:"zadigx_release_tag"`
 }
 
 type ContainerImage struct {
