b28fc714317aaee8e88a44459d438d3a2857dda6
M-Cosmosss <yuzhou@koderover.com>
2023-08-04 11:48:29 +0800

    fix
    Signed-off-by: M-Cosmosss <yuzhou@koderover.com>

--- pkg/microservice/aslan/core/workflow/handler/workflow_v4.go
+++ pkg/microservice/aslan/core/workflow/handler/workflow_v4.go
@@ -698,7 +698,7 @@ func GetBlueGreenServiceK8sServiceYaml(c *gin.Context) {
 	ctx := internalhandler.NewContext(c)
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
 
-	blueGreenServiceYaml, err := workflow.GetBlueGreenServiceK8sServiceYaml(c.Query("projectName"), c.Query("envName"), c.Query("serviceName"))
+	blueGreenServiceYaml, err := workflow.GetBlueGreenServiceK8sServiceYaml(c.Query("projectName"), c.Param("envName"), c.Param("serviceName"))
 	if err != nil {
 		ctx.Err = err
 		return
