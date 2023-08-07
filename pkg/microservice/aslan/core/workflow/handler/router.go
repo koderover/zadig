c8646ef55579daee7d9d1b1ab64436ff8585866c
Min Min <minmin@koderover.com>
2023-08-07 10:27:48 +0800

    revert api deletion
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/workflow/handler/router.go
+++ pkg/microservice/aslan/core/workflow/handler/router.go
@@ -256,7 +256,7 @@ func (*Router) Inject(router *gin.RouterGroup) {
 	{
 		plugin.GET("/template", ListPluginTemplates)
 		plugin.POST("", UpsertUserPluginRepository)
-		//plugin.POST("/enterprise", UpsertEnterprisePluginRepository)
+		plugin.POST("/enterprise", UpsertEnterprisePluginRepository)
 		plugin.GET("", ListUnofficalPluginRepositories)
 		plugin.DELETE("/:id", DeletePluginRepo)
 	}
