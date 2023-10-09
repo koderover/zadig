80f7c9180b3198bbf5ab0ae10d38d63a90374c5e
allenshen <shendongdong@koderover.com>
2023-10-09 15:59:19 +0800

    add svc resource parse logic
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/service/service/service.go
+++ pkg/microservice/aslan/core/service/service/service.go
@@ -59,6 +59,11 @@ import (
 	yamlutil "github.com/koderover/zadig/pkg/util/yaml"
 )
 
+type SvcResources struct {
+	Kind string `json:"kind"`
+	Name string `json:"name"`
+}
+
 type ServiceOption struct {
 	ServiceModules     []*ServiceModule                 `json:"service_module"`
 	SystemVariable     []*Variable                      `json:"system_variable"`
@@ -66,6 +71,7 @@ type ServiceOption struct {
 	ServiceVariableKVs []*commontypes.ServiceVariableKV `json:"service_variable_kvs"`
 	Yaml               string                           `json:"yaml"`
 	Service            *commonmodels.Service            `json:"service,omitempty"`
+	Resources          []*SvcResources                  `json:"resources"`
 }
 
 type ServiceModule struct {
@@ -161,7 +167,6 @@ func GetServiceOption(args *commonmodels.Service, log *zap.SugaredLogger) (*Serv
 			Value: ""},
 	}
 
-	//serviceOption.VariableYaml = getTemplateMergedVariables(args)
 	serviceOption.VariableYaml = args.VariableYaml
 	serviceOption.ServiceVariableKVs = args.ServiceVariableKVs
 
@@ -169,6 +174,23 @@ func GetServiceOption(args *commonmodels.Service, log *zap.SugaredLogger) (*Serv
 		args.Source == setting.SourceFromGerrit || args.Source == setting.SourceFromGitee {
 		serviceOption.Yaml = args.Yaml
 	}
+
+	serviceOption.Resources = make([]*SvcResources, 0)
+	renderedYaml, err := commonutil.RenderK8sSvcYamlStrict(args.Yaml, args.ProductName, args.ServiceName, args.VariableYaml)
+	if err != nil {
+		log.Errorf("failed to render k8s svc yaml: %s/%s, err: %s", args.ProductName, args.ServiceName, err)
+	}
+	renderedYaml = util.ReplaceWrapLine(renderedYaml)
+	yamlDataArray := util.SplitYaml(renderedYaml)
+	for _, yamlData := range yamlDataArray {
+		resKind := new(types.KubeResourceKind)
+		if err := yaml.Unmarshal([]byte(yamlData), &resKind); err != nil {
+			log.Errorf("unmarshal ResourceKind error: %v", err)
+			continue
+		}
+		serviceOption.Resources = append(serviceOption.Resources, &SvcResources{Kind: resKind.Kind, Name: resKind.Metadata.Name})
+	}
+
 	return serviceOption, nil
 }
 
