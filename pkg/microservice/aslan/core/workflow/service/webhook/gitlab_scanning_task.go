0f3e3e8bfb0d6f2d26402c71919f7086857affdb
shendongdong <shendongdong@koderover.com>
2023-10-23 16:35:40 +0800

    Bugfix/scanning webhook trigger (#3138)
    * fix service container image update logic
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * debug (#2983)
    
    Signed-off-by: Min Min <minmin@koderover.com>
    Co-authored-by: Min Min <minmin@koderover.com>
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    ---------
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    Signed-off-by: Min Min <minmin@koderover.com>
    Co-authored-by: Min Min <37430175+jamsman94@users.noreply.github.com>
    Co-authored-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/webhook/gitlab_scanning_task.go
+++ pkg/microservice/aslan/core/workflow/service/webhook/gitlab_scanning_task.go
@@ -166,8 +166,15 @@ func (gpem *gitlabPushEventMatcherForScanning) Match(hookRepo *commonmodels.Scan
 		if !EventConfigured(matchRepo, config.HookEventPush) {
 			return false, nil
 		}
-		if hookRepo.Branch != getBranchFromRef(ev.Ref) {
-			return false, nil
+
+		if !matchRepo.IsRegular {
+			if getBranchFromRef(ev.Ref) != hookRepo.Branch {
+				return false, nil
+			}
+		} else {
+			if matched, _ := regexp.MatchString(hookRepo.Branch, getBranchFromRef(ev.Ref)); !matched {
+				return false, nil
+			}
 		}
 
 		hookRepo.Branch = getBranchFromRef(ev.Ref)
