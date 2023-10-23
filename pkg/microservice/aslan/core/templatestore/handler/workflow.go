27d25880c31dc9fdf95c27ba0f33901b5e51a739
Min Min <minmin@koderover.com>
2023-10-23 13:42:28 +0800

    misc logic update
    Signed-off-by: Min Min <minmin@koderover.com>

--- pkg/microservice/aslan/core/templatestore/handler/workflow.go
+++ pkg/microservice/aslan/core/templatestore/handler/workflow.go
@@ -104,7 +104,6 @@ func CreateWorkflowTemplate(c *gin.Context) {
 	defer func() { internalhandler.JSONResponse(c, ctx) }()
 
 	if err != nil {
-
 		ctx.Err = fmt.Errorf("authorization Info Generation failed: err %s", err)
 		ctx.UnAuthorized = true
 		return
