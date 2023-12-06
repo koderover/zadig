dfb99441cc94adab3afc8d062e9f60902db11884
shendongdong <shendongdong@koderover.com>
2023-12-06 10:08:45 +0800

    add resource check for host workload creation (#3228)
    * add resource check for host workload creation
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    * fix error
    
    Signed-off-by: allenshen <shendongdong@koderover.com>
    
    ---------
    
    Signed-off-by: allenshen <shendongdong@koderover.com>

--- pkg/microservice/aslan/core/environment/handler/environment.go
+++ pkg/microservice/aslan/core/environment/handler/environment.go
@@ -1047,7 +1047,7 @@ func UpdateHelmProductDefaultValues(c *gin.Context) {
 		return
 	}
 
-	if arg.ValuesData.AutoSync {
+	if arg.ValuesData != nil && arg.ValuesData.AutoSync {
 		if !commonutil.ValidateZadigXLicenseStatus(licenseStatus) {
 			ctx.Err = e.ErrLicenseInvalid.AddDesc("")
 			return
@@ -2387,6 +2387,24 @@ func ListWorkloads(c *gin.Context) {
 			workloadM[serviceInExternalEnv.ServiceName] = commonmodels.Workload{
 				EnvName:     serviceInExternalEnv.EnvName,
 				ProductName: serviceInExternalEnv.ProductName,
+			}
+		}
+
+		// find workloads existed in other envs
+		sharedNSEnvs, err := mongodb.NewProductColl().ListEnvByNamespace(args.ClusterID, args.Namespace)
+		if err != nil {
+			log.Errorf("ListWorkloads ListEnvByNamespace err:%v", err)
+		}
+		for _, env := range sharedNSEnvs {
+			for _, svc := range env.GetSvcList() {
+				for _, res := range svc.Resources {
+					if res.Kind == setting.Deployment || res.Kind == setting.StatefulSet {
+						workloadM[res.Name] = commonmodels.Workload{
+							EnvName:     env.EnvName,
+							ProductName: env.ProductName,
+						}
+					}
+				}
 			}
 		}
 
