61a3b5bd2debde55a03c87b95eb6f04e3ecdaafc
Petrus <zhaoyu@koderover.com>
2023-12-22 11:32:45 +0800

    add grayscale demo to examples (#3265)
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

---
+++ examples/grayscale-demo/cmd/myapp-1/app/options.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/spf13/pflag"
+)
+
+// Options is the configuration of demo.
+type Options struct {
+	ListenAddr     string
+	DownstreamAddr string
+	HttpClient     *http.Client
+	Headers        []string
+
+	PrintVersion bool
+}
+
+// NewOptions news configurations of demo.
+func NewOptions() *Options {
+	return &Options{
+		HttpClient: &http.Client{},
+	}
+}
+
+// AddFlags adds flag options.
+func (op *Options) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&op.ListenAddr, "listen-addr", "0.0.0.0:8080", "listen addr")
+	fs.StringVar(&op.DownstreamAddr, "downstream-addr", "0.0.0.0:8081", "addr of downstream")
+	fs.StringSliceVar(&op.Headers, "headers", []string{}, "http headers to propagate, such as 'x-name,x-age' or 'x-name'")
+
+	fs.BoolVar(&op.PrintVersion, "version", false, "Print version info and quit")
+}
