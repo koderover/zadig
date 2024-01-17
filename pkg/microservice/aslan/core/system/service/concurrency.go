c47294579f0d8148fd6265925395cf96bac6246e
Min Min <jamsman94@gmail.com>
2024-01-17 14:08:34 +0800

    remove scale wd logic when updating workflow concurrency setting
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/system/service/concurrency.go
+++ pkg/microservice/aslan/core/system/service/concurrency.go
@@ -21,14 +21,8 @@ import (
 
 	"go.uber.org/zap"
 
-	configbase "github.com/koderover/zadig/v2/pkg/config"
-	"github.com/koderover/zadig/v2/pkg/microservice/aslan/config"
 	commonrepo "github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/repository/mongodb"
 	workflowservice "github.com/koderover/zadig/v2/pkg/microservice/aslan/core/workflow/service/workflow"
-	"github.com/koderover/zadig/v2/pkg/setting"
-	kubeclient "github.com/koderover/zadig/v2/pkg/shared/kube/client"
-	e "github.com/koderover/zadig/v2/pkg/tool/errors"
-	"github.com/koderover/zadig/v2/pkg/tool/kube/updater"
 )
 
 func GetWorkflowConcurrency() (*WorkflowConcurrencySettings, error) {
@@ -54,10 +48,6 @@ func UpdateWorkflowConcurrency(workflowConcurrency, buildConcurrency int64, log 
 		log.Errorf("Failed to update system settings, the error is: %s", err)
 		return err
 	}
-	// then we update the warpdrive deployment, where the workflow concurrency happens
-	kubeClient, err := kubeclient.GetKubeClient(config.HubServerAddress(), setting.LocalClusterID)
-	if err != nil {
-		return e.ErrScaleService.AddErr(err)
-	}
-	return updater.ScaleDeployment(config.Namespace(), configbase.WarpDriveServiceName(), int(workflowConcurrency), kubeClient)
+
+	return nil
 }
