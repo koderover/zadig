5e7a8cdcbc25c096e47a4e0edc0c77de7dd0902a
Min Min <37430175+jamsman94@users.noreply.github.com>
2024-01-30 16:34:03 +0800

    fix errors (#3326)
    * fix errors
    
    Signed-off-by: Min Min <jamsman94@gmail.com>
    
    * debug
    
    Signed-off-by: Min Min <jamsman94@gmail.com>
    
    * debug
    
    Signed-off-by: Min Min <jamsman94@gmail.com>
    
    ---------
    
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/systemconfig/core/features/service/feature.go
+++ pkg/microservice/systemconfig/core/features/service/feature.go
@@ -19,6 +19,7 @@ package service
 import (
 	"context"
 
+	"github.com/koderover/zadig/v2/pkg/tool/cache"
 	"go.uber.org/zap"
 
 	"github.com/koderover/zadig/v2/pkg/microservice/systemconfig/core/features/repository/models"
@@ -26,6 +27,12 @@ import (
 )
 
 func FeatureEnabled(f string, log *zap.SugaredLogger) (bool, error) {
+	featureLock := cache.NewRedisLock("feature_gate")
+	featureLock.Lock()
+	defer func() {
+		featureLock.Unlock()
+	}()
+
 	features, err := mongodb.NewFeatureColl().ListFeatures()
 	if err != nil {
 		log.Errorf("failed to get feature:%s from the db, error: %s", f, err)
@@ -47,6 +54,12 @@ type FeatureReq struct {
 }
 
 func UpdateOrCreateFeature(req *FeatureReq, log *zap.SugaredLogger) error {
+	featureLock := cache.NewRedisLock("feature_gate")
+	featureLock.Lock()
+	defer func() {
+		featureLock.Unlock()
+	}()
+
 	err := mongodb.NewFeatureColl().UpdateOrCreateFeature(context.TODO(), &models.Feature{
 		Name:    req.Name,
 		Enabled: req.Enabled,
