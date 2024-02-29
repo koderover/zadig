ebb313ae5e98e77c15aad68ef27dfcc8ff9f8339
Patrick Zhao <zhaoyu@koderover.com>
2024-02-29 11:50:24 +0800

    remove waitgroup and goroutine
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_deploy.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_deploy.go
@@ -21,7 +21,6 @@ import (
 	"fmt"
 	"sort"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
@@ -436,17 +435,11 @@ BetaCronLoop:
 
 func (c *DeployJobCtl) updateServiceModuleImages(ctx context.Context, resources []*kube.WorkloadResource, env *commonmodels.Product) error {
 	errList := new(multierror.Error)
-	wg := sync.WaitGroup{}
 	for _, serviceModule := range c.jobTaskSpec.ServiceAndImages {
-		wg.Add(1)
-		go func(serviceModule *commonmodels.DeployServiceModule) {
-			defer wg.Done()
-			if err := c.updateExternalServiceModule(ctx, resources, env, serviceModule); err != nil {
-				errList = multierror.Append(errList, err)
-			}
-		}(serviceModule)
+		if err := c.updateExternalServiceModule(ctx, resources, env, serviceModule); err != nil {
+			errList = multierror.Append(errList, err)
+		}
 	}
-	wg.Wait()
 	if err := errList.ErrorOrNil(); err != nil {
 		return err
 	}
