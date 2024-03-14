4a27f92f7a4ae6d26bbc2d74810d1a0ffa40ef7a
lilianzhu <zhujiali@koderover.com>
2024-03-14 17:35:08 +0800

    add java-tomcat example
    Signed-off-by: lilianzhu <zhujiali@koderover.com>

---
+++ examples/java-tomcat-demo/src/main/java/launch/Main.java
@@ -0,0 +1,28 @@
+package launch;
+
+import java.io.File;
+import org.apache.catalina.startup.Tomcat;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
---
+++ examples/java-tomcat-demo/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h2>Hello world!</h2>
+    </body>
+</html>
