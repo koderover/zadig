3a4d37e9a05c7a9b039b26f7b15d21e39fcfebbb
allenshen <shendongdong@koderover.com>
2024-03-25 11:38:42 +0800

    add route

--- pkg/microservice/aslan/core/environment/handler/router.go
+++ pkg/microservice/aslan/core/environment/handler/router.go
@@ -232,7 +232,7 @@ func (*Router) Inject(router *gin.RouterGroup) {
 		production.GET("/environments/:name/istioGrayscale/portal/:serviceName", GetIstioGrayscalePortalService)
 		production.POST("/environments/:name/istioGrayscale/portal/:serviceName", SetupIstioGrayscalePortalService)
 
-		production.GET("/environments/operations", GetProductionOperationLogs)
+		production.GET("/operations", GetProductionOperationLogs)
 	}
 
 	// ---------------------------------------------------------------------------------------
