6a1d57f790a6fea181f959b8854791169630bf00
Min Min <jamsman94@gmail.com>
2024-04-15 17:31:40 +0800

    debug
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/release_plan/service/release_plan.go
+++ pkg/microservice/aslan/core/release_plan/service/release_plan.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/koderover/zadig/v2/pkg/microservice/aslan/core/workflow/service/workflow/job"
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 
@@ -30,7 +31,6 @@ import (
 	"github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/repository/models"
 	"github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/repository/mongodb"
 	approvalservice "github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/service/approval"
-	"github.com/koderover/zadig/v2/pkg/microservice/aslan/core/workflow/service/workflow/job"
 	"github.com/koderover/zadig/v2/pkg/setting"
 	"github.com/koderover/zadig/v2/pkg/shared/client/user"
 	"github.com/koderover/zadig/v2/pkg/shared/handler"
@@ -139,33 +139,35 @@ func GetReleasePlan(id string) (*models.ReleasePlan, error) {
 	}
 
 	for _, releasePlanJob := range releasePlan.Jobs {
-		spec := new(models.WorkflowReleaseJobSpec)
-		if err := models.IToi(releasePlanJob.Spec, spec); err != nil {
-			return nil, fmt.Errorf("invalid spec for job: %s. decode error: %s", releasePlanJob.Name, err)
-		}
-		if spec.Workflow == nil {
-			return nil, fmt.Errorf("workflow is nil")
-		}
+		if releasePlanJob.Type == config.JobWorkflow {
+			spec := new(models.WorkflowReleaseJobSpec)
+			if err := models.IToi(releasePlanJob.Spec, spec); err != nil {
+				return nil, fmt.Errorf("invalid spec for job: %s. decode error: %s", releasePlanJob.Name, err)
+			}
+			if spec.Workflow == nil {
+				return nil, fmt.Errorf("workflow is nil")
+			}
 
-		originalWorkflow, err := mongodb.NewWorkflowV4Coll().Find(spec.Workflow.Name)
-		if err != nil {
-			log.Errorf("Failed to find WorkflowV4: %s, the error is: %v", spec.Workflow.Name, err)
-			return nil, fmt.Errorf("failed to find WorkflowV4: %s, the error is: %v", spec.Workflow.Name, err)
-		}
+			originalWorkflow, err := mongodb.NewWorkflowV4Coll().Find(spec.Workflow.Name)
+			if err != nil {
+				log.Errorf("Failed to find WorkflowV4: %s, the error is: %v", spec.Workflow.Name, err)
+				return nil, fmt.Errorf("failed to find WorkflowV4: %s, the error is: %v", spec.Workflow.Name, err)
+			}
 
-		if err := job.MergeArgs(originalWorkflow, spec.Workflow); err != nil {
-			errMsg := fmt.Sprintf("merge workflow args error: %v", err)
-			log.Error(errMsg)
-			return nil, fmt.Errorf(errMsg)
-		}
+			if err := job.MergeArgs(originalWorkflow, spec.Workflow); err != nil {
+				errMsg := fmt.Sprintf("merge workflow args error: %v", err)
+				log.Error(errMsg)
+				return nil, fmt.Errorf(errMsg)
+			}
 
-		for _, stage := range originalWorkflow.Stages {
-			for _, item := range stage.Jobs {
-				err := job.SetOptions(item, originalWorkflow)
-				if err != nil {
-					errMsg := fmt.Sprintf("merge workflow args set options error: %v", err)
-					log.Error(errMsg)
-					return nil, fmt.Errorf(errMsg)
+			for _, stage := range originalWorkflow.Stages {
+				for _, item := range stage.Jobs {
+					err := job.SetOptions(item, originalWorkflow)
+					if err != nil {
+						errMsg := fmt.Sprintf("merge workflow args set options error: %v", err)
+						log.Error(errMsg)
+						return nil, fmt.Errorf(errMsg)
+					}
 				}
 			}
 		}
