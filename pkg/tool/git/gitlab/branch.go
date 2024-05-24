f16b9b511cd33b7bff90268f5334eb1687263e2c
Patrick Zhao <zhaoyu@koderover.com>
2024-05-24 17:29:37 +0800

    add debug log
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/tool/git/gitlab/branch.go
+++ pkg/tool/git/gitlab/branch.go
@@ -16,12 +16,17 @@ limitations under the License.
 
 package gitlab
 
-import "github.com/xanzy/go-gitlab"
+import (
+	"github.com/koderover/zadig/v2/pkg/tool/log"
+	"github.com/xanzy/go-gitlab"
+)
 
 // ListBranches lists branches by projectID <- urlEncode(namespace/projectName)
 func (c *Client) ListBranches(owner, repo, key string, opts *ListOptions) ([]*gitlab.Branch, error) {
 	branches, err := wrap(paginated(func(o *gitlab.ListOptions) ([]interface{}, *gitlab.Response, error) {
+		log.Debugf("opts: %+v", opts)
 		bs, r, err := c.Branches.ListBranches(generateProjectName(owner, repo), &gitlab.ListBranchesOptions{ListOptions: *o, Search: &key})
+		log.Debugf("count of branches: %d", len(bs))
 		var res []interface{}
 		for _, b := range bs {
 			res = append(res, b)
