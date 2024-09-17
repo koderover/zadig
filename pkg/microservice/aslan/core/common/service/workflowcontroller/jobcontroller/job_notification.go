d1ff73159d6f1614bfe2857282b18f49b6ff0829
Min MIn <jamsman94@gmail.com>
2024-09-17 13:32:23 +0800

    add dingding message logic
    Signed-off-by: Min MIn <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_notification.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_notification.go
@@ -62,7 +62,16 @@ func (c *NotificationJobCtl) Run(ctx context.Context) {
 	c.ack()
 
 	if c.jobTaskSpec.WebHookType == setting.NotifyWebHookTypeFeishu {
-		err := sendLarkMessage(c.workflowCtx.WorkflowName, c.workflowCtx.WorkflowDisplayName, c.workflowCtx.TaskID, c.jobTaskSpec.FeiShuWebHook, c.jobTaskSpec.Title, c.jobTaskSpec.Content, c.jobTaskSpec.LarkUserIDs, c.jobTaskSpec.IsAtAll)
+		err := sendLarkMessage(c.workflowCtx.ProjectName, c.workflowCtx.WorkflowName, c.workflowCtx.WorkflowDisplayName, c.workflowCtx.TaskID, c.jobTaskSpec.FeiShuWebHook, c.jobTaskSpec.Title, c.jobTaskSpec.Content, c.jobTaskSpec.LarkUserIDs, c.jobTaskSpec.IsAtAll)
+		if err != nil {
+			c.logger.Error(err)
+			c.job.Status = config.StatusFailed
+			c.job.Error = err.Error()
+			c.ack()
+			return
+		}
+	} else if c.jobTaskSpec.WebHookType == setting.NotifyWebHookTypeDingDing {
+		err := sendDingDingMessage(c.workflowCtx.ProjectName, c.workflowCtx.WorkflowName, c.workflowCtx.WorkflowDisplayName, c.workflowCtx.TaskID, c.jobTaskSpec.DingDingWebHook, c.jobTaskSpec.Title, c.jobTaskSpec.Content, c.jobTaskSpec.AtMobiles, c.jobTaskSpec.IsAtAll)
 		if err != nil {
 			c.logger.Error(err)
 			c.job.Status = config.StatusFailed
@@ -78,7 +87,7 @@ func (c *NotificationJobCtl) Run(ctx context.Context) {
 	return
 }
 
-func sendLarkMessage(workflowName, workflowDisplayName string, taskID int64, uri, title, message string, idList []string, isAtAll bool) error {
+func sendLarkMessage(productName, workflowName, workflowDisplayName string, taskID int64, uri, title, message string, idList []string, isAtAll bool) error {
 	// first generate lark card
 	card := instantmessage.NewLarkCard()
 	card.SetConfig(true)
@@ -89,8 +98,9 @@ func sendLarkMessage(workflowName, workflowDisplayName string, taskID int64, uri
 	)
 
 	card.AddI18NElementsZhcnFeild(message, true)
-	url := fmt.Sprintf("%s/v1/projects/detail/{{.Task.ProductName}}/pipelines/custom/%s/%d?display_name=%s",
+	url := fmt.Sprintf("%s/v1/projects/detail/%s/pipelines/custom/%s/%d?display_name=%s",
 		configbase.SystemAddress(),
+		productName,
 		workflowName,
 		taskID,
 		workflowDisplayName,
@@ -143,6 +153,47 @@ func sendLarkMessage(workflowName, workflowDisplayName string, taskID int64, uri
 		}
 	}
 	return nil
+}
+
+func sendDingDingMessage(productName, workflowName, workflowDisplayName string, taskID int64, uri, title, message string, idList []string, isAtAll bool) error {
+	processedMessage := generateGeneralNotificationMessage(productName, workflowName, workflowDisplayName, taskID, message)
+
+	messageReq := instantmessage.DingDingMessage{
+		MsgType: "markdown",
+		MarkDown: &instantmessage.DingDingMarkDown{
+			Title: title,
+			Text:  processedMessage,
+		},
+	}
+
+	messageReq.At = &instantmessage.DingDingAt{
+		AtMobiles: idList,
+		IsAtAll:   isAtAll,
+	}
+
+	// TODO: if required, add proxy to it
+	c := httpclient.New()
+
+	_, err := c.Post(uri, httpclient.SetBody(messageReq))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generateGeneralNotificationMessage(productName, workflowName, workflowDisplayName string, taskID int64, content string) string {
+	url := fmt.Sprintf("%s/v1/projects/detail/%s/pipelines/custom/%s/%d?display_name=%s",
+		configbase.SystemAddress(),
+		productName,
+		workflowName,
+		taskID,
+		workflowDisplayName,
+	)
+
+	resp := content + "\n"
+	resp += fmt.Sprintf("- 触发的工作流: %s\n", url)
+	return resp
 }
 
 func (c *NotificationJobCtl) SaveInfo(ctx context.Context) error {
