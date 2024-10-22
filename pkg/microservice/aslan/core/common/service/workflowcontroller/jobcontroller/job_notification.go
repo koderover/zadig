703f65c14cf1e4db1655d4342f79d4751e72e1c7
Min Min <jamsman94@gmail.com>
2024-10-22 15:11:32 +0800

    debug
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_notification.go
+++ pkg/microservice/aslan/core/common/service/workflowcontroller/jobcontroller/job_notification.go
@@ -341,7 +341,7 @@ func sendMailMessage(productName, workflowName, workflowDisplayName string, task
 }
 
 func generateDingDingNotificationMessage(title, content string) string {
-	titleStr := fmt.Sprintf("####<font color=#0000FF> %s", title)
+	titleStr := fmt.Sprintf("###<font color=#0000FF>11 %s</font>", title)
 
 	resp := fmt.Sprintf("%s\n%s", titleStr, content)
 	return resp
