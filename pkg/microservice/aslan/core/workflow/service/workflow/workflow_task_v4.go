5c9a2ed253873296b87ef9046f99f86b50b15965
Min Min <jamsman94@gmail.com>
2024-11-26 09:33:26 +0800

    added more ignore field in workflow hash
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/workflow_task_v4.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/workflow_task_v4.go
@@ -531,7 +531,6 @@ func CreateWorkflowTaskV4(args *CreateWorkflowTaskV4Args, workflow *commonmodels
 	workflowTask.WorkflowDisplayName = workflow.DisplayName
 	workflowTask.ProjectName = workflow.Project
 	workflowTask.Params = workflow.Params
-	workflowTask.KeyVals = workflow.KeyVals
 	workflowTask.ShareStorages = workflow.ShareStorages
 	workflowTask.IsDebug = workflow.Debug
 	workflowTask.Remark = workflow.Remark
