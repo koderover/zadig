5f39159d28d8735cadce3a7350a031e38458ef84
Min Min <jamsman94@gmail.com>
2024-12-30 14:14:03 +0800

    fix pointer access to avoid unexpected array value error
    Signed-off-by: Min Min <jamsman94@gmail.com>

---
+++ pkg/util/deep_copy.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2024 The KodeRover Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func DeepCopy(dst interface{}, src interface{}) error {
+	if dst == nil {
+		return fmt.Errorf("dst cannot be nil")
+	}
+	if src == nil {
+		return fmt.Errorf("src cannot be nil")
+	}
+	bytes, err := json.Marshal(src)
+	if err != nil {
+		return fmt.Errorf("unable to marshal src: %w", err)
+	}
+	err = json.Unmarshal(bytes, dst)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshal into dst: %w", err)
+	}
+	return nil
+}
