e7405414f954efac058ecd5c665862332bfa007c
Min Min <jamsman94@gmail.com>
2025-01-24 14:10:26 +0800

    add more debug logs
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/multicluster/service/clusters.go
+++ pkg/microservice/aslan/core/multicluster/service/clusters.go
@@ -18,6 +18,7 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -1602,6 +1603,11 @@ func GetClusterIRSAInfo(clusterID string, logger *zap.SugaredLogger) (*GetCluste
 }
 
 func stsHasImmutableFieldChanged(existing, desired *appsv1.StatefulSet) bool {
+	a, _ := json.Marshal(existing)
+	log.Infof("existing: \n %s \n ========================", string(a))
+	b, _ := json.Marshal(desired)
+	log.Infof("existing: \n %s \n ========================", string(b))
+
 	fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>> identical:", volumeClaimTemplateChanged(existing.Spec.VolumeClaimTemplates, desired.Spec.VolumeClaimTemplates))
 
 	// Example check for `volumeClaimTemplates`
