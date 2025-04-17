6654dd54141ad0c831492956635bc7ab0e904fd5
Min Min <jamsman94@gmail.com>
2025-04-17 10:42:25 +0800

    fix compile
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/controller/job/job_deploy.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/controller/job/job_deploy.go
@@ -243,7 +243,7 @@ func (j DeployJobController) Update(useUserInput bool, ticket *commonmodels.Appr
 						newUserKV = append(newUserKV, updatedKV)
 					}
 
-					mergedValues, err := helmtool.MergeOverrideValues("", variableInfo.VariableYaml, userSvc.VariableYaml, "", make([]*helmtool.KV, 0))
+					mergedValues, err := helmtool.MergeOverrideValues("", variableInfo.VariableYaml, service.VariableYaml, "", make([]*helmtool.KV, 0))
 					if err != nil {
 						return fmt.Errorf("failed to merge helm values, error: %s", err)
 					}
