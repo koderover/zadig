79ba73afc290c2b9edb1724c96b0bb526c7e3b56
Min Min <jamsman94@gmail.com>
2025-04-25 13:16:25 +0800

    add ua
    Signed-off-by: Min Min <jamsman94@gmail.com>

---
+++ pkg/cli/upgradeassistant/cmd/migrate/340.go
@@ -0,0 +1,224 @@
+/*
+Copyright 2025 The KodeRover Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package migrate
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/koderover/zadig/v2/pkg/cli/upgradeassistant/internal/upgradepath"
+	"github.com/koderover/zadig/v2/pkg/microservice/aslan/config"
+	commonmodels "github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/repository/models"
+	commonrepo "github.com/koderover/zadig/v2/pkg/microservice/aslan/core/common/repository/mongodb"
+	"github.com/koderover/zadig/v2/pkg/types"
+)
+
+func init() {
+	// 3.2.0 to 3.3.0
+	upgradepath.RegisterHandler("3.3.0", "3.4.0", V330ToV340)
+	upgradepath.RegisterHandler("3.4.0", "3.3.0", V340ToV330)
+}
+
+func V330ToV340() error {
+	workflowCursor, err := commonrepo.NewWorkflowV4Coll().ListByCursor(&commonrepo.ListWorkflowV4Option{})
+	if err != nil {
+		return fmt.Errorf("failed to list all custom workflow to update, error: %s", err)
+	}
+	for workflowCursor.Next(context.Background()) {
+		workflow := new(commonmodels.WorkflowV4)
+		if err := workflowCursor.Decode(workflow); err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func updateWorkflowJobTaskSpec(workflow *commonmodels.WorkflowV4) error {
+	for _, stage := range workflow.Stages {
+		for _, job := range stage.Jobs {
+			switch job.JobType {
+			case config.JobApollo:
+				newSpec := new(commonmodels.ApolloJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode apollo job, error: %s", err)
+				}
+				newSpec.NamespaceListOption = newSpec.NamespaceList
+				newSpec.NamespaceList = make([]*commonmodels.ApolloNamespace, 0)
+				job.Spec = newSpec
+			case config.JobK8sBlueGreenDeploy:
+				newSpec := new(commonmodels.BlueGreenDeployV2JobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.ServiceOptions = newSpec.Services
+				newSpec.Services = make([]*commonmodels.BlueGreenDeployV2Service, 0)
+				job.Spec = newSpec
+			case config.JobBuild:
+				newSpec := new(commonmodels.ZadigBuildJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.ServiceAndBuildsOptions = newSpec.ServiceAndBuilds
+				newSpec.ServiceAndBuilds = make([]*commonmodels.ServiceAndBuild, 0)
+				for _, svcBuildConfig := range newSpec.ServiceAndBuildsOptions {
+					updateKeyVal(svcBuildConfig.KeyVals)
+				}
+				job.Spec = newSpec
+			case config.JobK8sCanaryDeploy:
+				newSpec := new(commonmodels.CanaryDeployJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.TargetOptions = newSpec.Targets
+				newSpec.Targets = make([]*commonmodels.CanaryTarget, 0)
+				job.Spec = newSpec
+			case config.JobCustomDeploy:
+				newSpec := new(commonmodels.CustomDeployJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.TargetOptions = newSpec.Targets
+				newSpec.Targets = make([]*commonmodels.DeployTargets, 0)
+				job.Spec = newSpec
+			case config.JobZadigDeploy:
+				newSpec := new(commonmodels.ZadigDeployJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				if strings.HasPrefix(newSpec.Env, "<+fixed>") {
+					newSpec.Env = strings.TrimPrefix(newSpec.Env, "<+fixed>")
+					newSpec.EnvSource = config.ParamSourceFixed
+				} else {
+					newSpec.EnvSource = config.ParamSourceRuntime
+				}
+				job.Spec = newSpec
+			case config.JobFreestyle:
+				// TODO: Add freestyle job ua logic
+			case config.JobGrafana:
+				newSpec := new(commonmodels.GrafanaJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.AlertOptions = newSpec.Alerts
+				newSpec.Alerts = make([]*commonmodels.GrafanaAlert, 0)
+				job.Spec = newSpec
+			case config.JobK8sGrayRelease:
+				newSpec := new(commonmodels.GrayReleaseJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.TargetOptions = newSpec.Targets
+				newSpec.Targets = make([]*commonmodels.GrayReleaseTarget, 0)
+				job.Spec = newSpec
+			case config.JobJenkins:
+				newSpec := new(commonmodels.JenkinsJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.JobOptions = newSpec.Jobs
+				newSpec.Jobs = make([]*commonmodels.JenkinsJobInfo, 0)
+				job.Spec = newSpec
+			case config.JobK8sPatch:
+				newSpec := new(commonmodels.K8sPatchJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.PatchItemOptions = newSpec.PatchItems
+				newSpec.PatchItems = make([]*commonmodels.PatchItem, 0)
+				job.Spec = newSpec
+			case config.JobNacos:
+				newSpec := new(commonmodels.NacosJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newDefaultNacos := make([]*types.NacosDataID, 0)
+				for _, nacosData := range newSpec.NacosDatas {
+					newDefaultNacos = append(newDefaultNacos, &types.NacosDataID{
+						DataID: nacosData.DataID,
+						Group:  nacosData.Group,
+					})
+				}
+				newSpec.DefaultNacosDatas = newDefaultNacos
+				newSpec.NacosDatas = make([]*types.NacosConfig, 0)
+				if strings.HasPrefix(newSpec.NamespaceID, "<+fixed>") {
+					newSpec.NamespaceID = strings.TrimPrefix(newSpec.NamespaceID, "<+fixed>")
+					newSpec.Source = config.ParamSourceFixed
+				} else {
+					newSpec.Source = config.ParamSourceRuntime
+				}
+				job.Spec = newSpec
+			case config.JobZadigScanning:
+				newSpec := new(commonmodels.ZadigScanningJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.ScanningOptions = newSpec.Scannings
+				newSpec.Scannings = make([]*commonmodels.ScanningModule, 0)
+				newSpec.ServiceScanningOptions = newSpec.ServiceAndScannings
+				newSpec.ServiceAndScannings = make([]*commonmodels.ServiceAndScannings, 0)
+
+				for _, scanning := range newSpec.ScanningOptions {
+					updateKeyVal(scanning.KeyVals)
+				}
+
+				for _, svc := range newSpec.ServiceScanningOptions {
+					updateKeyVal(svc.KeyVals)
+				}
+				job.Spec = newSpec
+			case config.JobZadigTesting:
+				newSpec := new(commonmodels.ZadigTestingJobSpec)
+				if err := commonmodels.IToi(job.Spec, newSpec); err != nil {
+					return fmt.Errorf("failed to decode zadig build job, error: %s", err)
+				}
+				newSpec.TestModuleOptions = newSpec.TestModules
+				newSpec.TestModules = make([]*commonmodels.TestModule, 0)
+				newSpec.ServiceTestOptions = newSpec.ServiceAndTests
+				newSpec.ServiceAndTests = make([]*commonmodels.ServiceAndTest, 0)
+				for _, test := range newSpec.TestModuleOptions {
+					updateKeyVal(test.KeyVals)
+				}
+
+				for _, svc := range newSpec.ServiceTestOptions {
+					updateKeyVal(svc.KeyVals)
+				}
+				job.Spec = newSpec
+			default:
+			}
+		}
+	}
+	return nil
+}
+
+func updateKeyVal(kvs commonmodels.RuntimeKeyValList) {
+	for _, kv := range kvs {
+		if strings.HasPrefix(kv.Value, "<+fixed>") {
+			kv.Source = config.ParamSourceFixed
+			kv.Value = strings.TrimPrefix(kv.Value, "<+fixed>")
+		} else if strings.HasPrefix(kv.Value, "{{.") {
+			kv.Source = config.ParamSourceReference
+		} else {
+			kv.Source = config.ParamSourceRuntime
+		}
+	}
+}
+
+func V340ToV330() error {
+	return nil
+}
