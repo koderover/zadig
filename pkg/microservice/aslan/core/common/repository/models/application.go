abeb40287ea2512dc43f8376413341269e5c48a6
Min Min <jamsman94@gmail.com>
2025-09-09 15:35:44 +0800

    debug
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/common/repository/models/application.go
+++ pkg/microservice/aslan/core/common/repository/models/application.go
@@ -33,7 +33,7 @@ type Application struct {
 	Type                  string                    `bson:"type"                              json:"type"`
 	Owner                 string                    `bson:"owner"                             json:"owner"`
 	Language              string                    `bson:"language,omitempty"                json:"language,omitempty"`
-	CreateTime            int64                     `bson:"create_time"                       json:"create_time"`
+	CreateTime            int64                     `bson:"create_time,omitempty"             json:"create_time"`
 	UpdateTime            int64                     `bson:"update_time"                       json:"update_time"`
 	Description           string                    `bson:"description,omitempty"             json:"description,omitempty"`
 	TestingServiceName    string                    `bson:"testing_service_name,omitempty"    json:"testing_service_name,omitempty"`
