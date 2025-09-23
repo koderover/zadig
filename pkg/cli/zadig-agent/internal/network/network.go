3a8df13e42f21a2d11395308f2b89020ab1203ed
Min Min <jamsman94@gmail.com>
2025-09-23 16:23:48 +0800

    update agent logic
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/cli/zadig-agent/internal/network/network.go
+++ pkg/cli/zadig-agent/internal/network/network.go
@@ -112,13 +112,9 @@ type DownloadFileRequest struct {
 
 // DownloadFile downloads a file from the server using the file ID
 func (c *ZadigClient) DownloadFile(fileID, targetPath string) error {
-	downloadURL := GetFullURL(c.AgentConfig.URL, fmt.Sprintf(DownloadFileBaseUrl, fileID))
+	downloadURL := GetFullURL(c.AgentConfig.URL, fmt.Sprintf(DownloadFileBaseUrl+"?token=%s", fileID, c.AgentConfig.Token))
 
-	request := &DownloadFileRequest{
-		Token: c.AgentConfig.Token,
-	}
-
-	err := httpclient.Download(downloadURL, targetPath, httpclient.SetBody(request))
+	err := httpclient.Download(downloadURL, targetPath)
 	if err != nil {
 		return fmt.Errorf("failed to download file (ID: %s) to %s: %v", fileID, targetPath, err)
 	}
