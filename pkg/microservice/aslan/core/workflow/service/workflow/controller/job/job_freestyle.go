ec4ea6093bb83cd17d3f9523aa467cc61de56b72
Min Min <jamsman94@gmail.com>
2026-01-15 15:26:17 +0800

    debug logs update
    Signed-off-by: Min Min <jamsman94@gmail.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/controller/job/job_freestyle.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/controller/job/job_freestyle.go
@@ -631,7 +631,7 @@ func (j FreestyleJobController) generateSubTask(taskID int64, jobSubTaskID int, 
 		return nil, fmt.Errorf("failed to render service variables, error: %v", err)
 	}
 
-	fmt.Sprintf("renderedTask: %+v", renderedTask)
+	fmt.Printf("renderedTask: %+v\n", renderedTask)
 	return renderedTask, nil
 }
 
