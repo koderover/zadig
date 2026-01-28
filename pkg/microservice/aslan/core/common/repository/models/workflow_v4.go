af5a9c392d3a9e37e524dd06162104d4ea6da327
Patrick Zhao <zhaoyu@koderover.com>
2026-01-28 11:30:50 +0800

    support dynamic variable in plugin job
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/common/repository/models/workflow_v4.go
+++ pkg/microservice/aslan/core/common/repository/models/workflow_v4.go
@@ -1399,9 +1399,9 @@ type ApisixJobSpec struct {
 }
 
 type ApisixItemSpec struct {
-	Action       config.ApisixActionType `bson:"action"        json:"action"        yaml:"action"`
-	Type         config.ApisixItemType   `bson:"type"          json:"type"          yaml:"type"`
-	Spec         interface{}             `bson:"spec"          json:"spec"          yaml:"spec"`
+	Action config.ApisixActionType `bson:"action"        json:"action"        yaml:"action"`
+	Type   config.ApisixItemType   `bson:"type"          json:"type"          yaml:"type"`
+	Spec   interface{}             `bson:"spec"          json:"spec"          yaml:"spec"`
 }
 
 type PingCodeJobSpec struct {
@@ -1604,9 +1604,11 @@ type Param struct {
 	// support string/text/choice/repo/file type
 	ParamsType   string                 `bson:"type"                      json:"type"                        yaml:"type"`
 	Value        string                 `bson:"value"                     json:"value"                       yaml:"value,omitempty"`
-	Repo         *types.Repository      `bson:"repo"                     json:"repo"                         yaml:"repo,omitempty"`
+	Repo         *types.Repository      `bson:"repo"                      json:"repo"                        yaml:"repo,omitempty"`
 	ChoiceOption []string               `bson:"choice_option,omitempty"   json:"choice_option,omitempty"     yaml:"choice_option,omitempty"`
 	ChoiceValue  []string               `bson:"choice_value,omitempty"    json:"choice_value,omitempty"      yaml:"choice_value,omitempty"`
+	Script       string                 `bson:"script,omitempty"          json:"script,omitempty"            yaml:"script,omitempty"`
+	CallFunction string                 `bson:"call_function,omitempty"   json:"call_function,omitempty"     yaml:"call_function,omitempty"`
 	FileID       string                 `bson:"file_id,omitempty"         json:"file_id,omitempty"           yaml:"file_id,omitempty"`
 	FilePath     string                 `bson:"file_path,omitempty"       json:"file_path,omitempty"         yaml:"file_path,omitempty"`
 	Default      string                 `bson:"default"                   json:"default"                     yaml:"default"`
