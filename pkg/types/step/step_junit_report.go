13aee104a707b60769e0662e53366036c40fd444
Petrus <4990592+PetrusZ@users.noreply.github.com>
2026-02-09 18:05:47 +0800

    junit step support set pass rate (#4496)
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/types/step/step_junit_report.go
+++ pkg/types/step/step_junit_report.go
@@ -19,16 +19,17 @@ package step
 type StepJunitReportSpec struct {
 	SourceWorkflow string `bson:"source_workflow"           json:"source_workflow"                   yaml:"source_workflow"`
 	// no stage name is recorded since the job name is unique, for now (version 2.1.0)
-	SourceJobKey  string `bson:"source_job_key"             json:"source_job_key"                    yaml:"source_job_key"`
-	JobTaskName   string `bson:"job_task_name"              json:"job_task_name"                     yaml:"job_task_name"`
-	TaskID        int64  `bson:"task_id"                    json:"task_id"                           yaml:"task_id"`
-	ServiceName   string `bson:"service_name"               json:"service_name"                      yaml:"service_name"`
-	ServiceModule string `bson:"service_module"             json:"service_module"                    yaml:"service_module"`
-	ReportDir     string `bson:"report_dir"                 json:"report_dir"                        yaml:"report_dir"`
-	DestDir       string `bson:"dest_dir"                   json:"dest_dir"                          yaml:"dest_dir"`
-	S3DestDir     string `bson:"s3_dest_dir"                json:"s3_dest_dir"                       yaml:"s3_dest_dir"`
-	FileName      string `bson:"file_name"                  json:"file_name"                         yaml:"file_name"`
-	TestName      string `bson:"test_name"                  json:"test_name"                         yaml:"test_name"`
-	TestProject   string `bson:"test_project"               json:"test_project"                      yaml:"test_project"`
-	S3Storage     *S3    `bson:"s3_storage"                 json:"s3_storage"                        yaml:"s3_storage"`
+	SourceJobKey       string `bson:"source_job_key"             json:"source_job_key"                    yaml:"source_job_key"`
+	JobTaskName        string `bson:"job_task_name"              json:"job_task_name"                     yaml:"job_task_name"`
+	TaskID             int64  `bson:"task_id"                    json:"task_id"                           yaml:"task_id"`
+	ServiceName        string `bson:"service_name"               json:"service_name"                      yaml:"service_name"`
+	ServiceModule      string `bson:"service_module"             json:"service_module"                    yaml:"service_module"`
+	ReportDir          string `bson:"report_dir"                 json:"report_dir"                        yaml:"report_dir"`
+	DestDir            string `bson:"dest_dir"                   json:"dest_dir"                          yaml:"dest_dir"`
+	S3DestDir          string `bson:"s3_dest_dir"                json:"s3_dest_dir"                       yaml:"s3_dest_dir"`
+	FileName           string `bson:"file_name"                  json:"file_name"                         yaml:"file_name"`
+	TestName           string `bson:"test_name"                  json:"test_name"                         yaml:"test_name"`
+	TestProject        string `bson:"test_project"               json:"test_project"                      yaml:"test_project"`
+	TestResultPassRate int    `bson:"test_result_pass_rate"      json:"test_result_pass_rate"             yaml:"test_result_pass_rate"`
+	S3Storage          *S3    `bson:"s3_storage"                 json:"s3_storage"                        yaml:"s3_storage"`
 }
