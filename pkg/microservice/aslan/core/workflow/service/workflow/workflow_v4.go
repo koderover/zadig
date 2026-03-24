7bd725bef5d549d3e2ed826971f518b5779c305e
Patrick Zhao <zhaoyu@koderover.com>
2026-03-24 11:38:48 +0800

    add date time variable in workflow
    Signed-off-by: Patrick Zhao <zhaoyu@koderover.com>

--- pkg/microservice/aslan/core/workflow/service/workflow/workflow_v4.go
+++ pkg/microservice/aslan/core/workflow/service/workflow/workflow_v4.go
@@ -2599,6 +2599,7 @@ func getDefaultVars(workflow *commonmodels.WorkflowV4, currentJobName string) []
 	vars = append(vars, fmt.Sprintf(setting.RenderValueTemplate, "workflow.task.creator.id"))
 	vars = append(vars, fmt.Sprintf(setting.RenderValueTemplate, "workflow.task.creator.userId"))
 	vars = append(vars, fmt.Sprintf(setting.RenderValueTemplate, "workflow.task.timestamp"))
+	vars = append(vars, fmt.Sprintf(setting.RenderValueTemplate, "workflow.task.datetime"))
 	vars = append(vars, fmt.Sprintf(setting.RenderValueTemplate, "workflow.task.id"))
 	for _, param := range workflow.Params {
 		if param.ParamsType == "repo" || param.ParamsType == "file" {
