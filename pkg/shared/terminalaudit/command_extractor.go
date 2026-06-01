d98d67a3cb8cf0040356b3124b9aab139f50c4f7
huanghongbo-hhb <huanghongbo@koderover.com>
2026-06-01 10:05:56 +0800

    feat: improve bracketed paste command extraction
    Signed-off-by: huanghongbo-hhb <huanghongbo@koderover.com>

--- pkg/shared/terminalaudit/command_extractor.go
+++ pkg/shared/terminalaudit/command_extractor.go
@@ -1,8 +1,14 @@
 package terminalaudit
 
 import (
+	"bytes"
 	"strings"
 	"time"
+)
+
+var (
+	bracketedPasteStart = []byte{0x1b, '[', '2', '0', '0', '~'}
+	bracketedPasteEnd   = []byte{0x1b, '[', '2', '0', '1', '~'}
 )
 
 type ExtractedCommand struct {
@@ -12,10 +18,12 @@ type ExtractedCommand struct {
 }
 
 type CommandExtractor struct {
-	buffer           []byte
-	seq              int64
-	inEscape         bool
-	escapeBodyBegins bool
+	buffer            []byte
+	seq               int64
+	inEscape          bool
+	escapeBuffer      []byte
+	inBracketedPaste  bool
+	pasteEscapeBuffer []byte
 }
 
 func NewCommandExtractor() *CommandExtractor {
@@ -26,45 +34,130 @@ func (e *CommandExtractor) Consume(data string, offset time.Duration) []Extracte
 	commands := make([]ExtractedCommand, 0)
 	for i := 0; i < len(data); i++ {
 		ch := data[i]
-		if e.inEscape {
-			if !e.escapeBodyBegins && (ch == '[' || ch == ']' || ch == 'O' || ch == 'P') {
-				e.escapeBodyBegins = true
-				continue
-			}
-			if isEscapeTerminator(ch) {
-				e.inEscape = false
-				e.escapeBodyBegins = false
-			}
+		if e.inBracketedPaste {
+			commands = e.consumeBracketedPasteByte(ch, offset, commands)
 			continue
 		}
 
-		switch ch {
-		case 0x1b:
-			e.inEscape = true
-			e.escapeBodyBegins = false
-		case '\r', '\n':
-			command := strings.TrimSpace(string(e.buffer))
-			e.buffer = e.buffer[:0]
-			if command == "" {
-				continue
-			}
-			e.seq++
-			commands = append(commands, ExtractedCommand{
-				Seq:          e.seq,
-				Command:      command,
-				TimeOffsetMS: offset.Milliseconds(),
-			})
-		case 0x08, 0x7f:
-			if len(e.buffer) > 0 {
-				e.buffer = e.buffer[:len(e.buffer)-1]
-			}
-		default:
-			if ch >= 0x20 || ch == '\t' {
-				e.buffer = append(e.buffer, ch)
-			}
+		if e.inEscape {
+			commands = e.consumeEscapeByte(ch, offset, commands)
+			continue
+		}
+
+		commands = e.consumePlainByte(ch, offset, commands)
+	}
+	return commands
+}
+
+func (e *CommandExtractor) consumePlainByte(ch byte, offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	switch ch {
+	case 0x1b:
+		e.inEscape = true
+		e.escapeBuffer = append(e.escapeBuffer[:0], ch)
+	case '\r', '\n':
+		commands = e.flushCommand(offset, commands)
+	case 0x08, 0x7f:
+		if len(e.buffer) > 0 {
+			e.buffer = e.buffer[:len(e.buffer)-1]
+		}
+	default:
+		if ch >= 0x20 || ch == '\t' {
+			e.buffer = append(e.buffer, ch)
 		}
 	}
 	return commands
+}
+
+func (e *CommandExtractor) consumeEscapeByte(ch byte, offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	e.escapeBuffer = append(e.escapeBuffer, ch)
+	if len(e.escapeBuffer) < 2 {
+		return commands
+	}
+
+	second := e.escapeBuffer[1]
+	if second != '[' && second != ']' && second != 'O' && second != 'P' {
+		e.resetEscape()
+		return commands
+	}
+	if len(e.escapeBuffer) == 2 {
+		return commands
+	}
+
+	if !isEscapeTerminator(ch) {
+		return commands
+	}
+
+	if bytes.Equal(e.escapeBuffer, bracketedPasteStart) {
+		e.inBracketedPaste = true
+		e.pasteEscapeBuffer = e.pasteEscapeBuffer[:0]
+	}
+	e.resetEscape()
+	return commands
+}
+
+func (e *CommandExtractor) consumeBracketedPasteByte(ch byte, offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	if len(e.pasteEscapeBuffer) > 0 {
+		return e.consumePasteEscapeByte(ch, offset, commands)
+	}
+	if ch == 0x1b {
+		e.pasteEscapeBuffer = append(e.pasteEscapeBuffer[:0], ch)
+		return commands
+	}
+	return e.consumePastedByte(ch, offset, commands)
+}
+
+func (e *CommandExtractor) consumePasteEscapeByte(ch byte, offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	e.pasteEscapeBuffer = append(e.pasteEscapeBuffer, ch)
+	if bytes.Equal(e.pasteEscapeBuffer, bracketedPasteEnd) {
+		e.inBracketedPaste = false
+		e.pasteEscapeBuffer = e.pasteEscapeBuffer[:0]
+		return commands
+	}
+	if bytes.HasPrefix(bracketedPasteEnd, e.pasteEscapeBuffer) {
+		return commands
+	}
+	for _, pasteCh := range e.pasteEscapeBuffer {
+		commands = e.consumePastedByte(pasteCh, offset, commands)
+	}
+	e.pasteEscapeBuffer = e.pasteEscapeBuffer[:0]
+	return commands
+}
+
+func (e *CommandExtractor) consumePastedByte(ch byte, offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	switch ch {
+	case 0x1b:
+		e.buffer = append(e.buffer, ch)
+	case '\r', '\n':
+		commands = e.flushCommand(offset, commands)
+	case 0x08, 0x7f:
+		if len(e.buffer) > 0 {
+			e.buffer = e.buffer[:len(e.buffer)-1]
+		}
+	default:
+		if ch >= 0x20 || ch == '\t' {
+			e.buffer = append(e.buffer, ch)
+		}
+	}
+	return commands
+}
+
+func (e *CommandExtractor) flushCommand(offset time.Duration, commands []ExtractedCommand) []ExtractedCommand {
+	command := strings.TrimSpace(string(e.buffer))
+	e.buffer = e.buffer[:0]
+	if command == "" {
+		return commands
+	}
+	e.seq++
+	return append(commands, ExtractedCommand{
+		Seq:          e.seq,
+		Command:      command,
+		TimeOffsetMS: offset.Milliseconds(),
+	})
+}
+
+func (e *CommandExtractor) resetEscape() {
+	e.inEscape = false
+	e.escapeBuffer = e.escapeBuffer[:0]
 }
 
 func isEscapeTerminator(ch byte) bool {
