94a3045c077e9cbd703a04023fedd979ac428907
huanghongbo-hhb <huanghongbo@koderover.com>
2026-06-12 11:27:51 +0800

    fix: skip empty release plan snapshot changes
    Signed-off-by: huanghongbo-hhb <huanghongbo@koderover.com>

--- pkg/microservice/aslan/core/release_plan/service/release_plan.go
+++ pkg/microservice/aslan/core/release_plan/service/release_plan.go
@@ -22,7 +22,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -375,7 +374,170 @@ func resolveReleasePlanLogBaseSnapshot(baseSnapshot interface{}, originalPlan *m
 }
 
 func hasReleasePlanSnapshotChanges(beforeSnapshot, afterSnapshot interface{}) bool {
-	return !reflect.DeepEqual(beforeSnapshot, afterSnapshot)
+	return !releasePlanSnapshotValuesEqual(beforeSnapshot, afterSnapshot)
+}
+
+func releasePlanSnapshotValuesEqual(left, right interface{}) bool {
+	switch leftValue := left.(type) {
+	case map[string]interface{}:
+		rightValue, ok := right.(map[string]interface{})
+		if !ok {
+			return isEmptyReleasePlanSnapshotValue(left) && isEmptyReleasePlanSnapshotValue(right)
+		}
+		return releasePlanSnapshotMapsEqual(leftValue, rightValue)
+	case []interface{}:
+		rightValue, ok := right.([]interface{})
+		if !ok {
+			return isEmptyReleasePlanSnapshotValue(left) && isEmptyReleasePlanSnapshotValue(right)
+		}
+		return releasePlanSnapshotListsEqual(leftValue, rightValue)
+	default:
+		switch right.(type) {
+		case map[string]interface{}, []interface{}:
+			return isEmptyReleasePlanSnapshotValue(left) && isEmptyReleasePlanSnapshotValue(right)
+		}
+	}
+
+	if isEmptyReleasePlanSnapshotScalarValue(left) && isEmptyReleasePlanSnapshotScalarValue(right) {
+		return true
+	}
+	leftNumber, leftIsNumber := releasePlanSnapshotNumber(left)
+	rightNumber, rightIsNumber := releasePlanSnapshotNumber(right)
+	if leftIsNumber || rightIsNumber {
+		return leftIsNumber && rightIsNumber && leftNumber == rightNumber
+	}
+
+	return left == right
+}
+
+func releasePlanSnapshotMapsEqual(left, right map[string]interface{}) bool {
+	for key, leftValue := range left {
+		rightValue, exists := right[key]
+		if !exists {
+			if !isEmptyReleasePlanSnapshotValue(leftValue) {
+				return false
+			}
+			continue
+		}
+		if !releasePlanSnapshotValuesEqual(leftValue, rightValue) {
+			return false
+		}
+	}
+
+	for key, rightValue := range right {
+		if _, exists := left[key]; exists {
+			continue
+		}
+		if !isEmptyReleasePlanSnapshotValue(rightValue) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func releasePlanSnapshotListsEqual(left, right []interface{}) bool {
+	leftIdx, rightIdx := 0, 0
+	for {
+		for leftIdx < len(left) && isEmptyReleasePlanSnapshotValue(left[leftIdx]) {
+			leftIdx++
+		}
+		for rightIdx < len(right) && isEmptyReleasePlanSnapshotValue(right[rightIdx]) {
+			rightIdx++
+		}
+		if leftIdx == len(left) || rightIdx == len(right) {
+			break
+		}
+		if !releasePlanSnapshotValuesEqual(left[leftIdx], right[rightIdx]) {
+			return false
+		}
+		leftIdx++
+		rightIdx++
+	}
+
+	for leftIdx < len(left) {
+		if !isEmptyReleasePlanSnapshotValue(left[leftIdx]) {
+			return false
+		}
+		leftIdx++
+	}
+	for rightIdx < len(right) {
+		if !isEmptyReleasePlanSnapshotValue(right[rightIdx]) {
+			return false
+		}
+		rightIdx++
+	}
+
+	return true
+}
+
+func isEmptyReleasePlanSnapshotValue(value interface{}) bool {
+	switch typedValue := value.(type) {
+	case map[string]interface{}:
+		for _, item := range typedValue {
+			if !isEmptyReleasePlanSnapshotValue(item) {
+				return false
+			}
+		}
+		return true
+	case []interface{}:
+		for _, item := range typedValue {
+			if !isEmptyReleasePlanSnapshotValue(item) {
+				return false
+			}
+		}
+		return true
+	default:
+		return isEmptyReleasePlanSnapshotScalarValue(value)
+	}
+}
+
+func isEmptyReleasePlanSnapshotScalarValue(value interface{}) bool {
+	switch typedValue := value.(type) {
+	case nil:
+		return true
+	case string:
+		return typedValue == ""
+	case bool:
+		return !typedValue
+	default:
+		number, ok := releasePlanSnapshotNumber(value)
+		return ok && number == 0
+	}
+}
+
+func releasePlanSnapshotNumber(value interface{}) (float64, bool) {
+	switch typedValue := value.(type) {
+	case int:
+		return float64(typedValue), true
+	case int8:
+		return float64(typedValue), true
+	case int16:
+		return float64(typedValue), true
+	case int32:
+		return float64(typedValue), true
+	case int64:
+		return float64(typedValue), true
+	case uint:
+		return float64(typedValue), true
+	case uint8:
+		return float64(typedValue), true
+	case uint16:
+		return float64(typedValue), true
+	case uint32:
+		return float64(typedValue), true
+	case uint64:
+		return float64(typedValue), true
+	case float32:
+		return float64(typedValue), true
+	case float64:
+		return typedValue, true
+	case json.Number:
+		number, err := typedValue.Float64()
+		return number, err == nil
+	default:
+		return 0, false
+	}
 }
 
 func DeleteReleasePlan(c *gin.Context, username, id string) error {
