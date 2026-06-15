e9e723f293b62d106f0396dcbc5e49fe2a84813f
Cynthia <chenchen@koderover.com>
2026-06-15 10:33:37 +0800

    add Jira issue status and transition APIs
    Signed-off-by: Cynthia <chenchen@koderover.com>

--- pkg/microservice/aslan/core/system/handler/router.go
+++ pkg/microservice/aslan/core/system/handler/router.go
@@ -375,6 +375,8 @@ func (*Router) Inject(router *gin.RouterGroup) {
 		pm.GET("/:id/jira/sprint/:sprintID/issue", ListJiraSprintIssues)
 		pm.GET("/:id/jira/issue", SearchJiraIssues)
 		pm.GET("/:id/jira/issue/jql", SearchJiraProjectIssuesWithJQL)
+		pm.GET("/:id/jira/issue/status", GetJiraIssueStatus)
+		pm.GET("/:id/jira/issue/transitions", ListJiraIssueTransitions)
 		pm.GET("/:id/jira/type", GetJiraTypes)
 		pm.GET("/:id/jira/status", GetJiraProjectStatus)
 		pm.GET("/:id/jira/allStatus", GetJiraAllStatus)
