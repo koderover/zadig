0afd60ccbfaff58c26156121b14fe405e4921706
YuTang Song <2313186065@qq.com>
2026-06-24 10:11:04 +0800

    fix: add log operation permission in openapi
    Signed-off-by: YuTang Song <2313186065@qq.com>

--- pkg/microservice/aslan/core/system/handler/openapi.go
+++ pkg/microservice/aslan/core/system/handler/openapi.go
@@ -65,7 +65,7 @@ func OpenAPICreateRegistry(c *gin.Context) {
 	c.Request.Body = io.NopCloser(bytes.NewBuffer(data))
 
 	// authorization checks
-	if !ctx.Resources.IsSystemAdmin && !ctx.Resources.SystemActions.LogOperation.View {
+	if !ctx.Resources.IsSystemAdmin {
 		ctx.UnAuthorized = true
 		return
 	}
@@ -119,7 +119,7 @@ func OpenAPIGetRegistry(c *gin.Context) {
 	}
 
 	// authorization checks
-	if !ctx.Resources.IsSystemAdmin && !ctx.Resources.SystemActions.LogOperation.View {
+	if !ctx.Resources.IsSystemAdmin {
 		ctx.UnAuthorized = true
 		return
 	}
@@ -476,7 +476,7 @@ func OpenAPIGetOperationLogs(c *gin.Context) {
 	}
 
 	// authorization checks
-	if !ctx.Resources.IsSystemAdmin {
+	if !ctx.Resources.IsSystemAdmin && !ctx.Resources.SystemActions.LogOperation.View {
 		ctx.UnAuthorized = true
 		return
 	}
